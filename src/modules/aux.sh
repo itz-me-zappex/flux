969c6a2c99e66dd5a3550a254a34d53e0865389b
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2024-11-27 16:23:35 +0200

    src/modules/aux.sh: Check for file readability in procfs in `get_process_info()`.

--- src/modules/aux.sh
+++ src/modules/aux.sh
@@ -88,30 +88,46 @@ get_process_info(){
 				message --verbose "Cache of parent window with ID $local_matching_window_id has been used to obtain info about window with ID $window_id and process '$process_name' with PID $process_pid."
 			else
 				# Extract name of process
-				process_name="$(<"/proc/$process_pid/comm")"
+				if check_ro "/proc/$process_pid/comm"; then
+					process_name="$(<"/proc/$process_pid/comm")"
+				else
+					return 2
+				fi
 				# Extract executable path of process
-				process_executable="$(readlink "/proc/$process_pid/exe")"
+				if check_ro "/proc/$process_pid/exe"; then
+					process_executable="$(readlink "/proc/$process_pid/exe")"
+				else
+					return 2
+				fi
 				# Extract effective UID of process
-				while read -r local_temp_status_line; do
-					# Find a line which contains UID
-					if [[ "$local_temp_status_line" == 'Uid:'* ]]; then
-						# Find 3rd column, which effective UID is
-						for local_status_column in $local_temp_status_line; do
-							# Current column number
-							(( local_column_count++ ))
-							# Remember UID and break cycle if that is effective UID
-							if (( local_column_count == 3 )); then
-								process_owner="$local_status_column"
-								break
-							fi
-						done
-					fi
-				done < "/proc/$process_pid/status"
+				if check_ro "/proc/$process_pid/status"; then
+					while read -r local_temp_status_line; do
+						# Find a line which contains UID
+						if [[ "$local_temp_status_line" == 'Uid:'* ]]; then
+							# Find 3rd column, which effective UID is
+							for local_status_column in $local_temp_status_line; do
+								# Current column number
+								(( local_column_count++ ))
+								# Remember UID and break cycle if that is effective UID
+								if (( local_column_count == 3 )); then
+									process_owner="$local_status_column"
+									break
+								fi
+							done
+						fi
+					done < "/proc/$process_pid/status"
+				else
+					return 2
+				fi
 				# I did not get how to do that using built-in bash options
 				# Extract command of process and replace '\0' (used as separator between options) with spaces
-				process_command="$(tr '\0' ' ' < "/proc/$process_pid/cmdline")"
-				# Remove last space because '\0' (which is replaced with space) is last symbol too
-				process_command="${process_command/%\ /}"
+				if check_ro "/proc/$process_pid/cmdline"; then
+					process_command="$(tr '\0' ' ' < "/proc/$process_pid/cmdline")"
+					# Remove last space because '\0' (which is replaced with space) is last symbol too
+					process_command="${process_command/%\ /}"
+				else
+					return 2
+				fi
 				message --verbose "Obtained info about window with ID $window_id and process '$process_name' with PID $process_pid has been cached."
 			fi
 			# Store info about window and process to cache
