3b765532564fb211c70cbcb29cd334a5dabea13a
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2024-12-02 17:21:58 +0200

    src/modules/background_fps_limit.sh: Rename function from `background_mangohud_fps_set()` to `background_fps_limit()`.

--- src/modules/background_fps_limit.sh
+++ src/modules/background_fps_limit.sh
@@ -1,5 +1,5 @@
 # Set specified FPS on unfocus, required to run it on background to avoid stopping a whole code if delay specified
-background_mangohud_fps_set(){
+background_fps_limit(){
 	# Wait for N seconds if delay is specified
 	if [[ "${config_key_delay_map["$passed_section"]}" != '0' ]]; then
 		message --verbose "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' will be FPS limited after ${config_key_delay_map["$passed_section"]} second(s) on unfocus event."
