cd48b8e08deba280ba4d04305572d204a4e4dd3a
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2024-12-02 16:48:42 +0200

    src/modules/: Split `background_set_limit.sh` into `background_cpulimit.sh`, `background_freeze_process.sh` and `background_mangohud_fps_set.sh`.

---
+++ src/modules/background_freeze_process.sh
@@ -0,0 +1,19 @@
+# Freeze process on unfocus event, required to run it on background to avoid stopping a whole code if delay specified
+background_freeze_process(){
+	# Wait for N seconds if delay is specified
+	if [[ "${config_key_delay_map["$passed_section"]}" != '0' ]]; then
+		message --verbose "Process '$passed_process_name' with PID $passed_process_pid will be frozen after ${config_key_delay_map["$passed_section"]} second(s) on unfocus event."
+		sleep "${config_key_delay_map["$passed_section"]}"
+	fi
+	# Check for process existence before freezing
+	if check_pid_existence "$passed_process_pid"; then
+		# Attempt to send 'SIGSTOP' signal to freeze process completely
+		if ! kill -STOP "$passed_process_pid" > /dev/null 2>&1; then
+			message --warning "Process '$passed_process_name' with PID $passed_process_pid cannot be frozen on unfocus event!"
+		else
+			message --info "Process '$passed_process_name' with PID $passed_process_pid has been frozen on unfocus event."
+		fi
+	else
+		message --warning "Process '$passed_process_name' with PID $passed_process_pid has been terminated before freezing!"
+	fi
+}
