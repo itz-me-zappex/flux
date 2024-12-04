a9f1a5fae84c7b348403228d25a42f15442ffd91
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2024-12-04 15:53:51 +0200

    src/modules/exec_unfocus.sh: Do not pass varibles with info about window, instead, use it directly in `exec_on_event()` from `aux.sh`.

--- src/modules/exec_unfocus.sh
+++ src/modules/exec_unfocus.sh
@@ -3,12 +3,6 @@ exec_unfocus(){
 	# Check for previous section match, existence of command in 'exec-unfocus' key, status of '--lazy' and bool related to unsetting of '--lazy'
 	if [[ -n "$previous_section" && -n "${config_key_exec_unfocus_map["$previous_section"]}" && -z "$lazy" && -z "$lazy_is_unset" ]]; then
 		# Execute command from 'exec-unfocus' key
-		passed_window_id="$previous_window_id" \
-		passed_process_pid="$previous_process_pid" \
-		passed_process_name="$previous_process_name" \
-		passed_process_executable="$previous_process_executable" \
-		passed_process_owner="$previous_process_owner" \
-		passed_process_command="$previous_process_command" \
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_exec_unfocus_map["$previous_section"]}" \
 		passed_event='unfocus' \
