c6ec77e6b961492a802411fca651275250fa27e1
itz-me-zappex <lospedloh7@gmail.com>
2024-12-04 15:52:07 +0200

    src/modules/exec_focus.sh: Do not pass varibles with info about window, instead, use it directly in `exec_on_event()` from `aux.sh`.

--- src/modules/exec_focus.sh
+++ src/modules/exec_focus.sh
@@ -3,12 +3,6 @@ exec_focus(){
 	# Execute command from 'exec-focus' key if section matches, specified 'exec-focus' key and that is not lazy mode
 	if [[ -n "${config_key_exec_focus_map["$section"]}" && -z "$lazy" ]]; then
 		# Execute command from 'exec-focus' key
-		passed_window_id="$window_id" \
-		passed_process_pid="$process_pid" \
-		passed_process_name="$process_name" \
-		passed_process_executable="$process_executable" \
-		passed_process_owner="$process_owner" \
-		passed_process_command="$process_command" \
 		passed_section="$section" \
 		passed_event_command="${config_key_exec_focus_map["$section"]}" \
 		passed_event='focus' \
