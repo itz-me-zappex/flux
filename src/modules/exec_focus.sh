8ed402e02fae630c3cdf177757c72de2454047bd
itz-me-zappex <lospedloh7@gmail.com>
2024-12-07 16:17:22 +0200

    src/modules/exec_focus.sh: Remove all mentions and cases where `--lazy` is used.

--- src/modules/exec_focus.sh
+++ src/modules/exec_focus.sh
@@ -1,7 +1,7 @@
 # Required to execute command from 'exec-focus' key in config
 exec_focus(){
 	# Execute command from 'exec-focus' key if section matches, specified 'exec-focus' key and that is not lazy mode
-	if [[ -n "${config_key_exec_focus_map["$section"]}" && -z "$lazy" ]]; then
+	if [[ -n "${config_key_exec_focus_map["$section"]}" ]]; then
 		# Execute command from 'exec-focus' key
 		passed_section="$section" \
 		passed_event_command="${config_key_exec_focus_map["$section"]}" \
