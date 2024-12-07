71b4f4b2ca49199e7868759baf60000ab570c576
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2024-12-07 17:19:09 +0200

    src/modules/exec_focus.sh: Execute commands from `lazy-exec-focus` if `--hot` is not set or has been unset after processing opened windows.

--- src/modules/exec_focus.sh
+++ src/modules/exec_focus.sh
@@ -1,10 +1,16 @@
-# Required to execute command from 'exec-focus' key in config
+# Required to execute command from 'exec-focus' and 'lazy-exec-focus' config keys
 exec_focus(){
-	# Execute command from 'exec-focus' key if section matches, specified 'exec-focus' key and that is not lazy mode
+	# Execute command from 'exec-focus' key if it has been specified
 	if [[ -n "${config_key_exec_focus_map["$section"]}" ]]; then
-		# Execute command from 'exec-focus' key
 		passed_section="$section" \
 		passed_event_command="${config_key_exec_focus_map["$section"]}" \
+		passed_event='focus' \
+		exec_on_event
+	fi
+	# Execute command from 'lazy-exec-focus' key if it has been specified and if '--hot' has been unset by daemon after processing opened windows
+	if [[ -n "${config_key_lazy_exec_focus_map["$section"]}" && -z "$hot"  ]]; then
+		passed_section="$section" \
+		passed_event_command="${config_key_lazy_exec_focus_map["$section"]}" \
 		passed_event='focus' \
 		exec_on_event
 	fi
