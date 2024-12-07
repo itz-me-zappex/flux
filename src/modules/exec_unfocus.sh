5e43f3eae324befa6a5793c9508ec375a1b0601e
itz-me-zappex <lospedloh7@gmail.com>
2024-12-07 17:19:55 +0200

    src/modules/exec_unfocus.sh: Execute commands from `lazy-exec-unfocus` if `--hot` is not set or has been unset after processing opened windows.

--- src/modules/exec_unfocus.sh
+++ src/modules/exec_unfocus.sh
@@ -1,11 +1,19 @@
-# Required to execute command from 'exec-unfocus' key in config
+# Required to execute command from 'exec-unfocus' and 'lazy-exec-unfocus' config keys
 exec_unfocus(){
-	# Check for previous section match, existence of command in 'exec-unfocus' key, status of '--lazy' and bool related to unsetting of '--lazy'
+	# Check for previous section match and execute command from 'exec-unfocus' key if it has been specified
 	if [[ -n "$previous_section" && -n "${config_key_exec_unfocus_map["$previous_section"]}" ]]; then
-		# Execute command from 'exec-unfocus' key
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_exec_unfocus_map["$previous_section"]}" \
 		passed_event='unfocus' \
 		exec_on_event
+	fi
+	# Check for previous section match and execute command from 'lazy-exec-unfocus' key if it has been specified and if '--hot' has been unset by daemon after processing opened windows
+	if [[ -n "$previous_section" && -n "${config_key_lazy_exec_unfocus_map["$previous_section"]}" && "$hot_is_unset" == '2' ]]; then
+		passed_section="$previous_section" \
+		passed_event_command="${config_key_lazy_exec_unfocus_map["$previous_section"]}" \
+		passed_event='unfocus' \
+		exec_on_event
+	elif [[ "$hot_is_unset" == '1' ]]; then # Needed to avoid execution of lazy unfocus command immediately after unsetting '--hot' option
+		hot_is_unset='2'
 	fi
 }
