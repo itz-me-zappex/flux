d3f7adb5794f582264ebf44f469654d66f185845
itz-me-zappex <lospedloh7@gmail.com>
2024-12-11 15:17:17 +0200

    src/modules/get_process_info.sh: Update comments.

--- src/modules/get_process_info.sh
+++ src/modules/get_process_info.sh
@@ -1,4 +1,4 @@
-# Required to get process info from cache in 'get_process_info' function
+# Required to get process info from cache
 cache_get_process_info(){
 	process_pid="${cache_process_pid_map["$passed_window_id"]}"
 	process_name="${cache_process_name_map["$passed_window_id"]}"
@@ -14,13 +14,13 @@ get_process_info(){
 	local_status_column \
 	local_matching_window_id \
 	local_temp_cached_window_id
-	# Use cache of window info if exists
+	# Use cache with window info if exists and is not bad
 	if [[ "${cache_event_type_map["$window_id"]}" == 'good' ]]; then
 		# Get process info from cache
 		passed_window_id="$window_id" cache_get_process_info
 		message --verbose "Cache has been used to obtain info about window with ID $window_id and process '$process_name' with PID $process_pid."
-	elif [[ -z "${cache_event_type_map["$window_id"]}" ]]; then # Extract process info if it is not cached
-		# Attempt to obtain output with PID
+	elif [[ -z "${cache_event_type_map["$window_id"]}" ]]; then # Get process info from procfs if not cached
+		# Obtain output with process PID using window ID
 		if ! process_pid="$(xprop -id "$window_id" _NET_WM_PID 2>/dev/null)" || [[ "$process_pid" == '_NET_WM_PID:  not found.' ]]; then
 			cache_event_type_map["$window_id"]='bad'
 			process_pid=''
@@ -39,7 +39,7 @@ get_process_info(){
 					break
 				fi
 			done
-			# Check for match of cached PID info to define a way how to obtain process info
+			# Check for match of cached process info to define a way how to obtain it
 			if [[ -n "$local_matching_window_id" ]]; then
 				# Get process info using cache of parent window
 				passed_window_id="$local_matching_window_id" cache_get_process_info
@@ -51,22 +51,22 @@ get_process_info(){
 				else
 					return 3
 				fi
-				# Extract name of process
+				# Get name of process
 				if check_ro "/proc/$process_pid/comm"; then
 					process_name="$(<"/proc/$process_pid/comm")"
 				else
 					return 2
 				fi
-				# Extract effective UID of process
+				# Get effective UID of process
 				if check_ro "/proc/$process_pid/status"; then
 					while read -r local_temp_status_line; do
 						# Find a line which contains UID
 						if [[ "$local_temp_status_line" == 'Uid:'* ]]; then
 							# Find 3rd column, which effective UID is
 							for local_status_column in $local_temp_status_line; do
-								# Current column number
+								# Increase column count
 								(( local_column_count++ ))
-								# Remember UID and break cycle if that is effective UID
+								# Remember effective UID and break loop (3rd column)
 								if (( local_column_count == 3 )); then
 									process_owner="$local_status_column"
 									break
@@ -77,18 +77,17 @@ get_process_info(){
 				else
 					return 2
 				fi
-				# I did not get how to do that using built-in bash options
-				# Extract command of process and replace '\0' (used as separator between options) with spaces
+				# Get command of process and replace '\0' (used as separator between options) with spaces
 				if check_ro "/proc/$process_pid/cmdline"; then
 					process_command="$(tr '\0' ' ' < "/proc/$process_pid/cmdline")"
-					# Remove last space because '\0' (which is replaced with space) is last symbol too
+					# Remove last space because '\0' which is replaced with space is last symbol too
 					process_command="${process_command/%\ /}"
 				else
 					return 2
 				fi
 				message --verbose "Obtained info about window with ID $window_id and process '$process_name' with PID $process_pid has been cached."
 			fi
-			# Store info about window and process to cache
+			# Associate info about window and process with cache-related associative arrays to use it next time
 			cache_event_type_map["$window_id"]='good'
 			cache_process_pid_map["$window_id"]="$process_pid"
 			cache_process_name_map["$window_id"]="$process_name"
