a9c98d7761301794ab20e2563c00e53c5f7665be
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2024-12-13 19:59:10 +0200

    src/modules/exec_focus.sh: Remove useless space from statement.

--- src/modules/exec_focus.sh
+++ src/modules/exec_focus.sh
@@ -8,7 +8,7 @@ exec_focus(){
 		exec_on_event
 	fi
 	# Execute command from 'lazy-exec-focus' key if it has been specified and if '--hot' has been unset by daemon after processing opened windows
-	if [[ -n "${config_key_lazy_exec_focus_map["$section"]}" && -z "$hot"  ]]; then
+	if [[ -n "${config_key_lazy_exec_focus_map["$section"]}" && -z "$hot" ]]; then
 		passed_section="$section" \
 		passed_event_command="${config_key_lazy_exec_focus_map["$section"]}" \
 		passed_event='focus' \
