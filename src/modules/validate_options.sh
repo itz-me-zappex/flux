95ed85518c503f80939b92fd62632435a1eb9ee8
itz-me-zappex <lospedloh7@gmail.com>
2024-12-13 23:06:17 +0200

    src/modules/validate_options.sh: Use `message --error-opt`.

--- src/modules/validate_options.sh
+++ src/modules/validate_options.sh
@@ -5,12 +5,12 @@ validate_options(){
 	local_new_prefix
 	# Exit with an error if verbose and quiet modes are specified at the same time
 	if [[ -n "$verbose" && -n "$quiet" ]]; then
-		message --error "Do not use verbose and quiet modes at the same time!$advice_on_option_error"
+		message --error-opt "Do not use verbose and quiet modes at the same time!"
 		exit 1
 	fi
 	# Exit with an error if '--log-overwrite' option is specified without '--log' option
 	if [[ -z "$log_is_passed" && -n "$log_overwrite" ]]; then
-		message --error "Do not use '--log-overwrite' without '--log' option!$advice_on_option_error"
+		message --error-opt "Do not use '--log-overwrite' without '--log' option!"
 		exit 1
 	fi
 	# Exit with an error if '--timestamp-format' is specified without '--timestamps'
@@ -20,7 +20,7 @@ validate_options(){
 	fi
 	# Exit with an error if '--config' option is specified without a path to config file
 	if [[ -n "$config_is_passed" && -z "$config" ]]; then
-		message --error "Option '--config' is specified without path to config file!$advice_on_option_error"
+		message --error-opt "Option '--config' is specified without path to config file!"
 		exit 1
 	fi
 	unset config_is_passed
@@ -31,13 +31,13 @@ validate_options(){
 		local_new_prefix="new_prefix_$local_temp_prefix_type"
 		# Exit with an error if option is passed but value does not exist
 		if [[ -n "${!local_is_passed}" && -z "${!local_new_prefix}" ]]; then
-			message --info "Option '--prefix-$local_temp_prefix_type' is specified without prefix!$advice_on_option_error"
+			message --error-opt "Option '--prefix-$local_temp_prefix_type' is specified without prefix!"
 			exit 1
 		fi
 	done
 	# Exit with an error if '--timestamp-format' option is specified without timestamp format
 	if [[ -n "$timestamp_is_passed" && -z "$new_timestamp_format" ]]; then
-		message --error "Option '--timestamp-format' is specified without timestamp format!$advice_on_option_error"
+		message --error-opt "Option '--timestamp-format' is specified without timestamp format!"
 		exit 1
 	fi
 	unset timestamp_is_passed
