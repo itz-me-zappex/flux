bc4036483a504647380b316e8c54f10970025409
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2024-12-18 20:42:51 +0200

    src/modules/exec_unfocus.sh: Pass variable with command type to define message on execution, specify full type of event to print it in message.

--- src/modules/exec_unfocus.sh
+++ src/modules/exec_unfocus.sh
@@ -15,16 +15,18 @@ exec_unfocus(){
 	FLUX_PROCESS_COMMAND="$previous_process_command"
 	# Check for previous section match and execute command from 'exec-unfocus' key if it has been specified
 	if [[ -n "$previous_section" && -n "${config_key_exec_unfocus_map["$previous_section"]}" ]]; then
+		passed_command_type='default' \
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_exec_unfocus_map["$previous_section"]}" \
-		passed_event='unfocus' \
+		passed_event='on unfocus event' \
 		exec_on_event
 	fi
 	# Check for previous section match and execute command from 'lazy-exec-unfocus' key if it has been specified and if '--hot' has been unset by daemon after processing opened windows
 	if [[ -n "$previous_section" && -n "${config_key_lazy_exec_unfocus_map["$previous_section"]}" && "$hot_is_unset" == '2' ]]; then
+		passed_command_type='lazy' \
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_lazy_exec_unfocus_map["$previous_section"]}" \
-		passed_event='unfocus' \
+		passed_event='on unfocus event' \
 		exec_on_event
 	elif [[ "$hot_is_unset" == '1' ]]; then # Needed to avoid execution of lazy unfocus command immediately after unsetting '--hot' option
 		hot_is_unset='2'
