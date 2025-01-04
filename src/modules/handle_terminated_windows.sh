ed6fe1bead35fa366cb6e5c1589c213db7110a0b
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-04 23:52:48 +0200

    src/modules/handle_terminated_windows.sh: Fix mistakes in variable names (`$*windows_ids` -> `$*window_ids`) and comments (`windows IDs` -> `window IDs`).

--- src/modules/handle_terminated_windows.sh
+++ src/modules/handle_terminated_windows.sh
@@ -6,7 +6,7 @@ unset_terminated_limits(){
 	local_temp_existing_window_id \
 	local_found
 	# Unset info about freezing and CPU limits of terminated windows
-	for local_temp_terminated_window_id in $local_terminated_windows_ids; do
+	for local_temp_terminated_window_id in $local_terminated_window_ids; do
 		# Skip window ID if that is bad event or info about it does not exist in cache
 		if [[ -n "${cache_event_type_map["$local_temp_terminated_window_id"]}" && "${cache_event_type_map["$local_temp_terminated_window_id"]}" != 'bad' ]]; then
 			# Obtain PID of terminated process using cache, required to check and unset FPS limit
@@ -27,7 +27,7 @@ unset_terminated_limits(){
 				unset_cpu_limit
 			elif [[ -n "${cache_section_map["$local_terminated_process_pid"]}" && -n "${is_fps_limited_section_map["${cache_section_map["$local_terminated_process_pid"]}"]}" ]]; then # Do not do anything if window is not FPS limited
 				# Do not remove FPS limit if one of existing windows matches with the same section
-				for local_temp_existing_window_id in $local_existing_windows_ids; do
+				for local_temp_existing_window_id in $local_existing_window_ids; do
 					# Obtain PID of terminated process using cache
 					local_existing_process_pid="${cache_process_pid_map["$local_temp_existing_window_id"]}"
 					# Mark to not unset FPS limit if there is another window which matches with same section
@@ -53,7 +53,7 @@ cache_collect_garbage(){
 	local_terminated_process_pid \
 	local_terminated_section
 	# Remove cached info about terminated windows
-	for local_temp_terminated_window_id in $local_terminated_windows_ids; do
+	for local_temp_terminated_window_id in $local_terminated_window_ids; do
 		# Check for event type before unset cache
 		if [[ "${cache_event_type_map["$local_temp_terminated_window_id"]}" == 'bad' ]]; then
 			# Unset only event type for bad window, otherwise bash will fail
@@ -91,11 +91,11 @@ cache_collect_garbage(){
 
 # Required to unset CPU/FPS limit for terminated windows and remove info about them from cache
 handle_terminated_windows(){
-	# Obtain list of terminated windows IDs
-	local local_terminated_windows_ids="${event/'terminated: '/}" # Remove everything before including type name of list with windows IDs
-	local local_terminated_windows_ids="${local_terminated_windows_ids/'; existing: '*/}" # Remove list of existing windows IDs
-	# Obtain list of existing windows IDs
-	local local_existing_windows_ids="${event/*'existing: '/}" # Remove everything including type name of list with windows IDs
+	# Obtain list of terminated window IDs
+	local local_terminated_window_ids="${event/'terminated: '/}" # Remove everything before including type name of list with window IDs
+	local local_terminated_window_ids="${local_terminated_window_ids/'; existing: '*/}" # Remove list of existing window IDs
+	# Obtain list of existing window IDs
+	local local_existing_window_ids="${event/*'existing: '/}" # Remove everything including type name of list with window IDs
 	# Unset CPU/FPS limit for terminated window, useful mostly to unset FPS limit in MangoHud config, but ability to unset all limits saves my ass from being burned
 	unset_terminated_limits
 	# Remove cached info about terminated windows
