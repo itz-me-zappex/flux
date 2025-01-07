598ec45af324bffe199e95e27281bc2ca9a94f48
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-07 03:52:40 +0200

    build.sh: Update modules list.

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ build(){
 		background_cpu_limit.sh
 		background_fps_limit.sh
 		background_freeze_process.sh
-		background_set_sched_idle.sh
+		background_sched_idle.sh
 		calculate_max_limit.sh
 		daemon_prepare.sh
 		event_source.sh
