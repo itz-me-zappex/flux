d8690431fc6064390bbf18838c00a3172bde9a80
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-07 04:32:06 +0200

    src/modules/actions_on_exit.sh: Restore scheduling policies on daemon termination if were changed to idle.

--- src/modules/actions_on_exit.sh
+++ src/modules/actions_on_exit.sh
@@ -2,7 +2,9 @@
 actions_on_exit(){
 	local local_temp_frozen_process_pid \
 	local_temp_cpulimit_bgprocess_pid \
-	local_temp_fps_limited_section
+	local_temp_fps_limited_section \
+	local_temp_idle_process_pid \
+	local_policy_option
 	# Unfreeze processes
 	for local_temp_frozen_process_pid in "${frozen_processes_pids_array[@]}"; do
 		# Check for existence of either delayed freezing background process or target process
@@ -30,10 +32,30 @@ actions_on_exit(){
 		# Set FPS from 'fps-focus' key to unset limit
 		mangohud_fps_set "${config_key_mangohud_config_map["$local_temp_fps_limited_section"]}" "${config_key_mangohud_source_config_map["$local_temp_fps_limited_section"]}" "${config_key_fps_focus_map["$local_temp_fps_limited_section"]}" > /dev/null 2>&1
 	done
-	# Remove lock file which prevents multiple instances of daemon from running
-	if [[ -f "$lock_file" ]] && ! rm "$lock_file" > /dev/null 2>&1; then
-		message --warning "Unable to remove lock file '$lock_file' which prevents multiple instances from running!"
-	fi
+	# Restore scheduling policies
+	for local_temp_idle_process_pid in "${idle_processes_pids_array[@]}"; do
+		# Check for existence of either delayed setting of idle scheduling policy for process or target process
+		if check_pid_existence "${set_sched_idle_bgprocess_pid_map["$local_temp_idle_process_pid"]}"; then
+			kill "${set_sched_idle_bgprocess_pid_map["$local_temp_idle_process_pid"]}"
+		elif check_pid_existence "$local_temp_idle_process_pid"; then
+			# Define option depending by previous scheduling policy
+			case "${sched_previous_policy_map["$local_temp_idle_process_pid"]}" in
+			'SCHED_FIFO' )
+				local_policy_option='--fifo'
+			;;
+			'SCHED_RR' )
+				local_policy_option='--rr'
+			;;
+			'SCHED_OTHER' )
+				local_policy_option='--other'
+			;;
+			'SCHED_BATCH' )
+				local_policy_option='--batch'
+			esac
+			# Restore scheduling policy and priority for process
+			chrt "$local_policy_option" --pid "${sched_previous_priority_map["$local_temp_idle_process_pid"]}" "$local_temp_idle_process_pid"
+		fi
+	done
 	# Execute command from 'lazy-exec-unfocus' if matching section for focused window is found and command is specified
 	if [[ -n "$previous_section" && -n "${config_key_lazy_exec_unfocus_map["$previous_section"]}" ]]; then
 		# Pass environment variables to interact with them using commands/scripts in 'lazy-exec-unfocus' config key
@@ -50,6 +72,10 @@ actions_on_exit(){
 		passed_event_command="${config_key_lazy_exec_unfocus_map["$previous_section"]}" \
 		passed_event='due to daemon termination' \
 		exec_on_event
+	fi
+	# Remove lock file which prevents multiple instances of daemon from running
+	if [[ -f "$lock_file" ]] && ! rm "$lock_file" > /dev/null 2>&1; then
+		message --warning "Unable to remove lock file '$lock_file' which prevents multiple instances from running!"
 	fi
 	# Wait a bit to avoid printing message about daemon termination earlier than messages from 'background_*()' appear
 	sleep 0.1
