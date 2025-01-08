86279a33905b85d43cb7fec462971560f4db9728
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-08 22:25:48 +0200

    src/modules/: Provide `unset_sched_idle.sh` module.

---
+++ src/modules/unset_sched_idle.sh
@@ -0,0 +1,88 @@
+# Required to terminate background process with delayed setting of 'SCHED_IDLE' or restore scheduling policy for process if window becomes focused or terminated
+unset_sched_idle(){
+	local local_sched_idle_bgprocess_pid \
+	local_print_delay_message \
+	local_policy_option \
+	local_policy_name \
+	local_temp_idle_pid \
+	local_idle_processes_pids_array
+	# Simplify access to PID of background process with delayed setting of 'SCHED_IDLE'
+	local_sched_idle_bgprocess_pid="${sched_idle_bgprocess_pid_map["$passed_process_pid"]}"
+	# Check for existence of background process with delayed setting of 'SCHED_IDLE'
+	if check_pid_existence "$local_sched_idle_bgprocess_pid"; then
+		# Attempt to terminate background process
+		if ! kill "$local_sched_idle_bgprocess_pid" > /dev/null 2>&1; then
+			local_print_delay_message='warning'
+		else
+			local_print_delay_message='info'
+		fi
+		# Print message if delay is not zero
+		if [[ "${config_key_delay_map["$passed_section"]}" != '0' ]]; then
+			# Define message depending by exit code of 'kill' command
+			case "$local_print_delay_message" in
+			'warning' )
+				message --warning "Unable to cancel delayed for $local_config_delay second(s) delayed setting of idle scheduling policy for process '$passed_process_name' with PID $passed_process_pid!"
+			;;
+			'info' )
+				message --info "Delayed for $local_config_delay second(s) setting of idle scheduling policy for process $passed_process_name' with PID $passed_process_pid has been cancelled $passed_end_of_msg."
+			esac
+		fi
+	else
+		# Define option and scheduling policy name depending by scheduling policy
+		case "${sched_previous_policy_map["$passed_process_pid"]}" in
+		'SCHED_FIFO' )
+			local_policy_option='--fifo'
+			local_policy_name='FIFO (first in first out)'
+		;;
+		'SCHED_RR' )
+			local_policy_option='--rr'
+			local_policy_name='RR (round robin)'
+		;;
+		'SCHED_OTHER' )
+			local_policy_option='--other'
+			local_policy_name='other'
+		;;
+		'SCHED_BATCH' )
+			local_policy_option='--batch'
+			local_policy_name='batch'
+		;;
+		'SCHED_DEADLINE' ) # Setting option unneeded because command for deadline differs greatly
+			local_policy_name='deadline'
+		esac
+		# Define how to restore scheduling policy depending by whether that is deadline or not
+		if [[ "${sched_previous_policy_map["$passed_process_pid"]}" == 'SCHED_DEADLINE' ]]; then
+			# Restore deadline scheduling policy and its parameters for process
+			chrt --deadline \
+			--sched-runtime "${sched_previous_runtime_map["$passed_process_pid"]}" \
+			--sched-deadline "${sched_previous_deadline_map["$passed_process_pid"]}" \
+			--sched-period "${sched_previous_period_map["$passed_process_pid"]}" \
+			--pid 0 "$passed_process_pid" > /dev/null 2>&1
+		else
+			# Attempt to restore scheduling policy and priority for process
+			chrt "$local_policy_option" --pid "${sched_previous_priority_map["$passed_process_pid"]}" "$passed_process_pid" > /dev/null 2>&1
+		fi
+		# Print message depending by 'chrt' exit code
+		if (( $? > 0 )); then
+			message --warning "Unable to restore $local_policy_name scheduling policy for process '$passed_process_name' with PID $passed_process_pid $passed_end_of_msg!"
+		else
+			message --info "Scheduling policy $local_policy_name has been restored for process '$passed_process_name' with PID $passed_process_pid $passed_end_of_msg."
+		fi
+		# Remove process PID from array
+		for local_temp_idle_pid in "${idle_processes_pids_array[@]}"; do
+			# Skip current PID as I want remove it from array
+			if [[ "$local_temp_idle_pid" != "$passed_process_pid" ]]; then
+				local_idle_processes_pids_array+=("$local_temp_idle_pid")
+			fi
+		done
+		# Store updated info info array
+		idle_processes_pids_array=("${local_idle_processes_pids_array[@]}")
+		# Unset details about previous and applied idle cheduling policies
+		sched_previous_policy_map["$passed_process_pid"]=''
+		sched_previous_priority_map["$passed_process_pid"]=''
+		sched_previous_runtime_map["$passed_process_pid"]=''
+		sched_previous_deadline_map["$passed_process_pid"]=''
+		sched_previous_period_map["$passed_process_pid"]=''
+		is_sched_idle_map["$passed_process_pid"]=''
+		sched_idle_bgprocess_pid_map["$passed_process_pid"]=''
+	fi
+}
