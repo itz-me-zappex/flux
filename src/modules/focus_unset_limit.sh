f9f718dc459a5f59815acced9153419377d0c7d7
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-08 22:28:30 +0200

    src/modules/focus_unset_limit.sh: Rename `$local_passed_end_of_msg` to `$local_end_of_msg`.

--- src/modules/focus_unset_limit.sh
+++ src/modules/focus_unset_limit.sh
@@ -1,12 +1,12 @@
 # Required to unset CPU/FPS limit for focused process
 focus_unset_limit(){
-	local local_passed_end_of_msg='on focus event'
+	local local_end_of_msg='on focus event'
 	# Unfreeze process if it has been frozen
 	if [[ -n "${is_frozen_pid_map["$process_pid"]}" ]]; then
 		passed_process_pid="$process_pid" \
 		passed_section="$section" \
 		passed_process_name="$process_name" \
-		passed_end_of_msg="$local_passed_end_of_msg" \
+		passed_end_of_msg="$local_end_of_msg" \
 		unfreeze_process
 	elif [[ -n "${is_cpu_limited_pid_map["$process_pid"]}" ]]; then # Unset CPU limit if has been applied
 		# Unset CPU limit
@@ -17,7 +17,7 @@ focus_unset_limit(){
 	elif [[ -n "${config_key_mangohud_config_map["$section"]}" ]]; then # Unset FPS limit or update target config on focus
 		# Unset FPS limit
 		passed_section="$section" \
-		passed_end_of_msg="$local_passed_end_of_msg" \
+		passed_end_of_msg="$local_end_of_msg" \
 		unset_fps_limit
 	fi
 	# Restore scheduling policy for process if it has been changed to idle
@@ -25,7 +25,7 @@ focus_unset_limit(){
 		passed_process_pid="$process_pid" \
 		passed_section="$section" \
 		passed_process_name="$process_name" \
-		passed_end_of_msg="$local_passed_end_of_msg" \
+		passed_end_of_msg="$local_end_of_msg" \
 		unset_sched_idle
 	fi
 }
