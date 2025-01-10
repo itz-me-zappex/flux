cdb77d326e1d1c9c7d2fe481a5a1c8fbfdd5feaf
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-10 02:17:53 +0200

    src/modules/actions_on_exit.sh: Rewrite limits unsetting algorithm.

--- src/modules/actions_on_exit.sh
+++ src/modules/actions_on_exit.sh
@@ -1,70 +1,47 @@
-# TODO: Rewrite this garbage from scratch and forget it as nigthmare.
-# Required to unset limits and run command from 'lazy-exec-unfocus' config key on SIGTERM or SIGINT signal
+# Required to unset applied limits for windows on SIGTERM/SIGINT signal
 actions_on_exit(){
-	local local_temp_frozen_process_pid \
-	local_temp_background_cpu_limit_pid \
-	local_temp_fps_limited_section \
-	local_temp_idle_process_pid \
-	local_policy_option
-	# Unfreeze processes
-	for local_temp_frozen_process_pid in "${frozen_processes_pids_array[@]}"; do
-		# Check for existence of either delayed freezing background process or target process
-		if check_pid_existence "${background_freeze_pid_map["$local_temp_frozen_process_pid"]}"; then
-			# Terminate background process if exists
-			kill "${background_freeze_pid_map["$local_temp_frozen_process_pid"]}" > /dev/null 2>&1
-		elif check_pid_existence "$local_temp_frozen_process_pid"; then
-			# Unfreeze process
-			kill -CONT "$local_temp_frozen_process_pid" > /dev/null 2>&1
+	local local_end_of_msg \
+	local_temp_window_id \
+	local_process_pid \
+	local_section \
+	local_process_name
+	# Define reason of unsetting limit to be displayed in messages
+	local_end_of_msg="due to daemon termination"
+	# Get list of all cached windows
+	for local_temp_window_id in "${!cache_process_pid_map[@]}"; do
+		# Simplify access to process PID
+		local_process_pid="${cache_process_pid_map["$local_temp_window_id"]}"
+		# Simplify access to matching section of process
+		local_section="${cache_section_map["$local_process_pid"]}"
+		# Simplify access to process name
+		local_process_name="${cache_process_name_map["$local_temp_window_id"]}"
+		# Define type of limit which should be unset
+		if [[ -n "${freeze_applied_map["$local_process_pid"]}" ]]; then
+			# Unfreeze process if has been frozen
+			passed_process_pid="$local_process_pid" \
+			passed_section="$local_section" \
+			passed_process_name="$local_process_name" \
+			passed_end_of_msg="$local_end_of_msg" \
+			unfreeze_process
+		elif [[ -n "${cpu_limit_applied_map["$local_process_pid"]}" ]]; then
+			# Unset CPU limit if has been applied
+			passed_process_pid="$local_process_pid" \
+			passed_process_name="$local_process_name" \
+			passed_signal='-SIGTERM' \
+			unset_cpu_limit
+		elif [[ -n "$local_section" && -n "${config_key_mangohud_config_map["$local_section"]}" ]]; then
+			# Unset FPS limit or update target config
+			passed_section="$local_section" \
+			passed_end_of_msg="$local_end_of_msg" \
+			unset_fps_limit
 		fi
-	done
-	# Unset CPU limits
-	for local_temp_background_cpu_limit_pid in "${background_cpu_limit_pids_array[@]}"; do
-		# Terminate 'cpulimit' process which has been started by daemon if exists
-		if check_pid_existence "$local_temp_background_cpu_limit_pid"; then
-			kill "$local_temp_background_cpu_limit_pid" > /dev/null 2>&1
-		fi
-	done
-	# Unset FPS limits
-	for local_temp_fps_limited_section in "${fps_limited_sections_array[@]}"; do
-		# Terminate process with delayed setting of FPS limit if exists
-		if check_pid_existence "${background_fps_limit_pid_map["$local_temp_fps_limited_section"]}"; then
-			kill "${background_fps_limit_pid_map["$local_temp_fps_limited_section"]}" > /dev/null 2>&1
-		fi
-		# Set FPS from 'fps-focus' key to unset limit
-		mangohud_fps_set "${config_key_mangohud_config_map["$local_temp_fps_limited_section"]}" "${config_key_mangohud_source_config_map["$local_temp_fps_limited_section"]}" "${config_key_fps_focus_map["$local_temp_fps_limited_section"]}" > /dev/null 2>&1
-	done
-	# Restore scheduling policies
-	for local_temp_idle_process_pid in "${idle_processes_pids_array[@]}"; do
-		# Check for existence of either delayed setting of idle scheduling policy for process or target process
-		if check_pid_existence "${background_sched_idle_pid_map["$local_temp_idle_process_pid"]}"; then
-			kill "${background_sched_idle_pid_map["$local_temp_idle_process_pid"]}" > /dev/null 2>&1
-		elif check_pid_existence "$local_temp_idle_process_pid"; then
-			# Define how to restore scheduling policy depending by whether that is deadline or not
-			if [[ "${sched_previous_policy_map["$local_temp_idle_process_pid"]}" != 'SCHED_DEADLINE' ]]; then
-				# Define option depending by scheduling policy
-				case "${sched_previous_policy_map["$local_temp_idle_process_pid"]}" in
-				'SCHED_FIFO' )
-					local_policy_option='--fifo'
-				;;
-				'SCHED_RR' )
-					local_policy_option='--rr'
-				;;
-				'SCHED_OTHER' )
-					local_policy_option='--other'
-				;;
-				'SCHED_BATCH' )
-					local_policy_option='--batch'
-				esac
-				# Restore scheduling policy and priority for process
-				chrt "$local_policy_option" --pid "${sched_previous_priority_map["$local_temp_idle_process_pid"]}" "$local_temp_idle_process_pid" > /dev/null 2>&1
-			else
-				# Restore deadline scheduling policy and its parameters for process
-				chrt --deadline \
-				--sched-runtime "${sched_previous_runtime_map["$local_temp_idle_process_pid"]}" \
-				--sched-deadline "${sched_previous_deadline_map["$local_temp_idle_process_pid"]}" \
-				--sched-period "${sched_previous_period_map["$local_temp_idle_process_pid"]}" \
-				--pid 0 "$local_temp_idle_process_pid" > /dev/null 2>&1
-			fi
+		# Restore scheduling policy for process if it has been changed to idle
+		if [[ -n "${sched_idle_applied_map["$local_process_pid"]}" ]]; then
+			passed_process_pid="$local_process_pid" \
+			passed_section="$local_section" \
+			passed_process_name="$local_process_name" \
+			passed_end_of_msg="$local_end_of_msg" \
+			unset_sched_idle
 		fi
 	done
 	# Execute command from 'lazy-exec-unfocus' if matching section for focused window is found and command is specified
