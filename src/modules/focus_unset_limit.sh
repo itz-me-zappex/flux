18ef3ffe6022278a9643ff534cbc6bb30012ea0e
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-10 02:02:18 +0200

    src/modules/focus_unset_limit.sh: Update comments.

--- src/modules/focus_unset_limit.sh
+++ src/modules/focus_unset_limit.sh
@@ -1,21 +1,22 @@
 # Required to unset limit for focused process
 focus_unset_limit(){
 	local local_end_of_msg='on focus event'
-	# Unfreeze process if it has been frozen
+	# Define type of limit which should be unset
 	if [[ -n "${freeze_applied_map["$process_pid"]}" ]]; then
+		# Unfreeze process if has been frozen
 		passed_process_pid="$process_pid" \
 		passed_section="$section" \
 		passed_process_name="$process_name" \
 		passed_end_of_msg="$local_end_of_msg" \
 		unfreeze_process
-	elif [[ -n "${cpu_limit_applied_map["$process_pid"]}" ]]; then # Unset CPU limit if has been applied
-		# Unset CPU limit
+	elif [[ -n "${cpu_limit_applied_map["$process_pid"]}" ]]; then
+		# Unset CPU limit if has been applied
 		passed_process_pid="$process_pid" \
 		passed_process_name="$process_name" \
 		passed_signal='-SIGUSR1' \
 		unset_cpu_limit
-	elif [[ -n "${config_key_mangohud_config_map["$section"]}" ]]; then # Unset FPS limit or update target config on focus
-		# Unset FPS limit
+	elif [[ -n "${config_key_mangohud_config_map["$section"]}" ]]; then
+		# Unset FPS limit or update target config
 		passed_section="$section" \
 		passed_end_of_msg="$local_end_of_msg" \
 		unset_fps_limit
