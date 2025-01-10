17417a74b1f70362d4b086d353f4696d10ca18ff
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-11 01:20:54 +0200

    src/modules/print_template.sh: Simplify X11 session check.

--- src/modules/print_template.sh
+++ src/modules/print_template.sh
@@ -1,46 +1,38 @@
 # Required to print info about focused/picked window in compatible with config way
 print_template(){
-	# Check for X11 session
+	# Exit with an error if X11 session is invalid
 	if ! x11_session_check; then
-		# Fail if something wrong with X server
-		fail='1'
+		if [[ "$1" =~ ^('--focus'|'-f')$ ]]; then
+			message --error "Unable to get info about focused window, something is wrong with X11 session or window manager is EMHW incompatible!"
+			exit 1
+		else
+			message --error "Unable to trigger window picker, something is wrong with X11 session or window manager is EMHW incompatible!"
+			exit 1
+		fi
 	fi
 	# Define way to obtain info about window depending by type of option
 	case "$1" in
 	--focus | -f )
-		# Check for failure related to X server check
-		if [[ -n "$fail" ]]; then
-			# Exit with an error if something wrong with X server
-			message --error "Unable to get info about focused window, something is wrong with X11 session or window manager is EMHW incompatible!"
-			exit 1
-		else
-			# Get output of 'xprop' tool containing window ID
-			window_id="$(xprop -root _NET_ACTIVE_WINDOW 2>/dev/null)"
-			# Extract ID of focused window
-			window_id="${window_id/*\# /}"
-		fi
+		# Get output of 'xprop' tool containing window ID
+		window_id="$(xprop -root _NET_ACTIVE_WINDOW 2>/dev/null)"
+		# Extract ID of focused window
+		window_id="${window_id/*\# /}"
 	;;
 	--pick | -p )
-		# Exit with an error if something wrong with X server
-		if [[ -n "$fail" ]]; then
-			message --error "Unable to trigger window picker, something is wrong with X11 session or window manager is EMHW incompatible!"
+		# Get output of 'xwininfo' tool containing window ID
+		if ! xwininfo_output="$(xwininfo 2>/dev/null)"; then
+			message --error "Unable to grab cursor to pick a window!"
 			exit 1
 		else
-			# Get output of 'xwininfo' tool containing window ID
-			if ! xwininfo_output="$(xwininfo 2>/dev/null)"; then
-				message --error "Unable to grab cursor to pick a window!"
-				exit 1
-			else
-				# Extract ID of focused window from output
-				while read -r temp_xwininfo_output_line; do
-					if [[ "$temp_xwininfo_output_line" == 'xwininfo: Window id: '* ]]; then
-						window_id="${temp_xwininfo_output_line/xwininfo: Window id: /}"
-						window_id="${window_id/ */}"
-						break
-					fi
-				done <<< "$xwininfo_output"
-				unset temp_xwininfo_output_line
-			fi
+			# Extract ID of focused window from output
+			while read -r temp_xwininfo_output_line; do
+				if [[ "$temp_xwininfo_output_line" == 'xwininfo: Window id: '* ]]; then
+					window_id="${temp_xwininfo_output_line/xwininfo: Window id: /}"
+					window_id="${window_id/ */}"
+					break
+				fi
+			done <<< "$xwininfo_output"
+			unset temp_xwininfo_output_line
 		fi
 	esac
 	# Get process info and print it in compatible with config way
