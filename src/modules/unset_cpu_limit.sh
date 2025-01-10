10036ae23e704a4d133e1bf731472f5d786a2bf4
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-10 02:14:14 +0200

    src/modules/unset_cpu_limit.sh: Remove warning and do not check exit code of `kill` command, because it may fail only if `background_cpu_limit()` already terminated (interrupted implicitly).

--- src/modules/unset_cpu_limit.sh
+++ src/modules/unset_cpu_limit.sh
@@ -3,9 +3,7 @@ unset_cpu_limit(){
 	local local_temp_background_cpu_limit_pid \
 	local_background_cpu_limit_pids_array
 	# Attempt to terminate CPU limit background process
-	if ! kill "$passed_signal" "${background_cpu_limit_pid_map["$passed_process_pid"]}" > /dev/null 2>&1; then
-		message --warning "Process '$passed_process_name' with PID $passed_process_pid cannot be CPU unlimited!"
-	fi
+	kill "$passed_signal" "${background_cpu_limit_pid_map["$passed_process_pid"]}" > /dev/null 2>&1
 	# Remove PID of 'cpulimit' background process from array
 	for local_temp_background_cpu_limit_pid in "${background_cpu_limit_pids_array[@]}"; do
 		# Skip interrupted background process as I want remove it from array
