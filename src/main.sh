0c0da6579020989b887a2c296c23652417796791
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-14 03:58:18 +0200

    src/main.sh: Validate X11 session by checking environment variables instead of calling `xprop`.

--- src/main.sh
+++ src/main.sh
@@ -103,117 +103,114 @@ sched_previous_runtime_map \
 sched_previous_deadline_map \
 sched_previous_period_map
 
-# Exit with an error if that is not a X11 session
-if ! x11_session_check; then
-	# Exit with an error if X11 session is invalid
-	message --error "Unable to start daemon, something is wrong with X11 session or window manager is EMHW incompatible!"
+# Exit with an error if X11 session is invalid
+if [[ "$XDG_SESSION_TYPE" != 'x11' || ! "$DISPLAY" =~ ^:[0-9](\.[0-9]+)?$ ]]; then
+	message --error "Unable to start daemon, something is wrong with X11 session!"
 	exit 1
-else
-	# Will not be used anymore
-	unset -f x11_session_check
-	# Preparation for event reading
-	daemon_prepare
-	unset -f daemon_prepare
-	# Read events
-	while read -r event; do
-		# These functions are not needed anymore since reading from 'event_source()' subprocess has been started
-		# And I can not unset them before start of 'event_source()'
-		if [[ -z "$event_source_is_unset" ]]; then
-			unset -f check_windows \
-			on_hot \
-			event_source
-			event_source_is_unset='1'
+fi
+
+# Preparation for event reading
+daemon_prepare
+unset -f daemon_prepare
+# Read events
+while read -r event; do
+	# These functions are not needed anymore since reading from 'event_source()' subprocess has been started
+	# And I can not unset them before start of 'event_source()'
+	if [[ -z "$event_source_is_unset" ]]; then
+		unset -f check_windows \
+		on_hot \
+		event_source
+		event_source_is_unset='1'
+	fi
+	# Apply actions depending by event type
+	case "$event" in
+	'error' )
+		# Exit with an error in case 'error' event appears
+		actions_on_exit
+		message --error "Flux has been terminated unexpectedly!"
+		exit 1
+	;;
+	'unset_hot' )
+		# Unset '--hot' as it becomes useless from this moment
+		unset hot
+		# Needed to make commands from 'lazy-exec-unfocus' keys work properly, 'exec_unfocus()' skips execution 'lazy-exec-unfocus' first time and increases value to '2'
+		hot_is_unset='1'
+	;;
+	'terminated'* )
+		# Unset CPU/FPS limits for terminated windows and remove info about them from cache
+		handle_terminated_windows
+	;;
+	'check_requests'* )
+		# Apply CPU/FPS limits for process which have been requested to be limited
+		set_requested_limits
+	;;
+	'restart' )
+		# Prepare daemon to reapply limits on 'event_source()' restart event which appears if list of window IDs becomes blank
+		hot='1'
+		unset hot_is_unset
+		# Unset info about processes to avoid using it by accident
+		unset window_id \
+		process_pid \
+		process_name \
+		process_owner \
+		process_command \
+		section \
+		previous_window_id \
+		previous_process_pid \
+		previous_process_name \
+		previous_process_owner \
+		previous_process_command \
+		previous_section
+	;;
+	* )
+		# Get window ID
+		window_id="${event/'='*/}"
+		# Get process PID of focused window
+		process_pid="${event/*'='/}"
+		# Attempt to obtain info about process using window ID
+		get_process_info
+		get_process_info_exit_code="$?"
+		# Request CPU/FPS limit for unfocused process if it matches with section
+		unfocus_request_limit
+		# Actions depending by exit code of 'get_process_info()'
+		if (( get_process_info_exit_code == 0 )); then
+			# Find matching section for process in config
+			if find_matching_section; then
+				# Unset CPU/FPS limit for focused process if it has been limited on unfocus
+				focus_unset_limit
+				# Execute command on focus event if specified in config
+				exec_focus
+			fi
+		else
+			message --warning "Unable to obtain info about process with PID $process_pid! Probably process has been terminated during check."
 		fi
-		# Apply actions depending by event type
-		case "$event" in
-		'error' )
-			# Exit with an error in case 'error' event appears
-			actions_on_exit
-			message --error "Flux has been terminated unexpectedly!"
-			exit 1
-		;;
-		'unset_hot' )
-			# Unset '--hot' as it becomes useless from this moment
-			unset hot
-			# Needed to make commands from 'lazy-exec-unfocus' keys work properly, 'exec_unfocus()' skips execution 'lazy-exec-unfocus' first time and increases value to '2'
-			hot_is_unset='1'
-		;;
-		'terminated'* )
-			# Unset CPU/FPS limits for terminated windows and remove info about them from cache
-			handle_terminated_windows
-		;;
-		'check_requests'* )
-			# Apply CPU/FPS limits for process which have been requested to be limited
-			set_requested_limits
-		;;
-		'restart' )
-			# Prepare daemon to reapply limits on 'event_source()' restart event which appears if list of window IDs becomes blank
-			hot='1'
-			unset hot_is_unset
-			# Unset info about processes to avoid using it by accident
-			unset window_id \
-			process_pid \
-			process_name \
-			process_owner \
-			process_command \
-			section \
-			previous_window_id \
+		# Execute command on unfocus event if specified in config
+		exec_unfocus
+		# Define what to do with info about previous window depending by exit code (overwrite or unset)
+		if (( get_process_info_exit_code == 0 )); then
+			# Remember info about process for next event to run commands on unfocus event and apply CPU/FPS limit, also for pass variables to command in 'exec-unfocus' key
+			previous_window_id="$window_id"
+			previous_process_pid="$process_pid"
+			previous_process_name="$process_name"
+			previous_process_owner="$process_owner"
+			previous_process_command="$process_command"
+			previous_section="$section"
+		else
+			# Forget info about previous window/process because it is not changed
+			unset previous_window_id \
 			previous_process_pid \
 			previous_process_name \
 			previous_process_owner \
 			previous_process_command \
 			previous_section
-		;;
-		* )
-			# Get window ID
-			window_id="${event/'='*/}"
-			# Get process PID of focused window
-			process_pid="${event/*'='/}"
-			# Attempt to obtain info about process using window ID
-			get_process_info
-			get_process_info_exit_code="$?"
-			# Request CPU/FPS limit for unfocused process if it matches with section
-			unfocus_request_limit
-			# Actions depending by exit code of 'get_process_info()'
-			if (( get_process_info_exit_code == 0 )); then
-				# Find matching section for process in config
-				if find_matching_section; then
-					# Unset CPU/FPS limit for focused process if it has been limited on unfocus
-					focus_unset_limit
-					# Execute command on focus event if specified in config
-					exec_focus
-				fi
-			else
-				message --warning "Unable to obtain info about process with PID $process_pid! Probably process has been terminated during check."
-			fi
-			# Execute command on unfocus event if specified in config
-			exec_unfocus
-			# Define what to do with info about previous window depending by exit code (overwrite or unset)
-			if (( get_process_info_exit_code == 0 )); then
-				# Remember info about process for next event to run commands on unfocus event and apply CPU/FPS limit, also for pass variables to command in 'exec-unfocus' key
-				previous_window_id="$window_id"
-				previous_process_pid="$process_pid"
-				previous_process_name="$process_name"
-				previous_process_owner="$process_owner"
-				previous_process_command="$process_command"
-				previous_section="$section"
-			else
-				# Forget info about previous window/process because it is not changed
-				unset previous_window_id \
-				previous_process_pid \
-				previous_process_name \
-				previous_process_owner \
-				previous_process_command \
-				previous_section
-			fi
-			unset get_process_info_exit_code
-			# Unset info about process to avoid using it by an accident
-			unset window_id \
-			process_pid \
-			process_name \
-			process_owner \
-			process_command \
-			section
-		esac
-	done < <(event_source)
-fi
+		fi
+		unset get_process_info_exit_code
+		# Unset info about process to avoid using it by an accident
+		unset window_id \
+		process_pid \
+		process_name \
+		process_owner \
+		process_command \
+		section
+	esac
+done < <(event_source)
