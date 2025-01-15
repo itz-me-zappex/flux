aa34b80af81732bc0e0f80f70d20886e63926b11
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-15 03:03:16 +0200

    src/modules/bash/event_source.sh: Remove workaround to reapply limits on WM restart, that is not an issue anymore.

--- src/modules/bash/event_source.sh
+++ src/modules/bash/event_source.sh
@@ -1,27 +1,3 @@
-# Required to check for window(s) existence
-check_windows(){
-	local local_event \
-	local_events_count='0'
-	# Do nothing if that is first cycle
-	if [[ -z "$first_cycle" ]]; then
-		message --warning "Opened windows were not found, waiting for their appearance…"
-		# Wait for windows appearance
-		while read -r local_event; do
-			(( local_events_count++ ))
-			# Check windows list
-			if (( local_events_count == 2 )); then
-				# Break loop if list with opened windows is not blank
-				if [[ -n "$local_event" ]]; then
-					break
-				else
-					# Reset events count
-					local_events_count='0'
-				fi
-			fi
-		done < <("$flux_event_reader" 2>/dev/null)
-	fi
-}
-
 # Required to get list of opened windows and print window IDs line by line if '--hot' option is specified
 # To make daemon apply limits to them and run commands from 'exec-focus' and 'exec-unfocus' config keys
 on_hot(){
@@ -74,17 +50,6 @@ event_reader(){
 		else
 			local_opened_windows="$local_event"
 		fi
-		# Break loop if list of windows appears blank
-		if [[ -z "$local_event" ]]; then
-			# Print event to prepare daemon for restart
-			echo 'restart'
-			# Set '--hot' to apply limits again as those have been unset because of X11 events nature
-			hot='1'
-			# Mark required to avoid loop breakage misunderstood as event reader crash
-			local_restart='1'
-			# Break loop
-			break
-		fi
 		# Print info about focused window as event
 		echo "$local_focused_window"
 		# Find terminated windows and store those to an array
@@ -113,22 +78,14 @@ event_reader(){
 	fi
 }
 
-# Required to send events to loop in 'flux' executable which reads events from this function
+# Required to send events to 'flux' which reads events from this function
 event_source(){
-	local first_cycle='1'
-	# Infinite loop required to make daemon able to restart event reading if list of windows becomes blank
-	# For example happens on Cinnamon when DE restarts and in pure Openbox environment
-	while :; do
-		# Check for window(s) existence
-		check_windows
-		unset first_cycle
-		# Print opened window IDs if '--hot' is specified
-		on_hot
-		# Handle events
-		if ! event_reader; then
-			message --warning "Event reader has been terminated!"
-			echo 'error'
-			break
-		fi
-	done
+	# Print opened window IDs as events if '--hot' is specified
+	on_hot
+	# Handle events
+	if ! event_reader; then
+		message --warning "Event reader has been terminated!"
+		echo 'error'
+		break
+	fi
 }
