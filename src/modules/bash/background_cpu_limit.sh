ee677b59d6d7d47d2b9cbe3e7c7bbd423909cffa
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-16 22:44:52 +0200

    src/modules/bash/background_cpu_limit.sh: Print window IDs in messages.

--- src/modules/bash/background_cpu_limit.sh
+++ src/modules/bash/background_cpu_limit.sh
@@ -4,7 +4,7 @@ background_cpu_limit(){
 	local_sleep_pid
 	# Wait before set limit and notify user if delay is specified
 	if [[ "${config_key_delay_map["$passed_section"]}" != '0' ]]; then
-		message --verbose "Process '$passed_process_name' with PID $passed_process_pid will be CPU limited after ${config_key_delay_map["$passed_section"]} second(s) on unfocus event."
+		message --verbose "Process '$passed_process_name' with PID $passed_process_pid will be CPU limited after ${config_key_delay_map["$passed_section"]} second(s) due to window $passed_window_id unfocus event."
 		# Run in background to make subprocess interruptable
 		sleep "${config_key_delay_map["$passed_section"]}" &
 		# Remember PID of 'sleep' to terminate it if needed
@@ -14,11 +14,11 @@ background_cpu_limit(){
 		kill "$local_sleep_pid"; \
 		exit 0' SIGINT SIGTERM
 		# Print relevant message on focus event and stop this subprocess
-		trap 'message --info "Delayed for ${config_key_delay_map["$passed_section"]} second(s) CPU limiting of process '"'$passed_process_name'"' with PID $passed_process_pid has been cancelled on focus event." ; \
+		trap 'message --info "Delayed for ${config_key_delay_map["$passed_section"]} second(s) CPU limiting of process '"'$passed_process_name'"' with PID $passed_process_pid has been cancelled due to window $passed_window_id focus event." ; \
 		kill "$local_sleep_pid"; \
 		exit 0' SIGUSR1
 		# Print relevant message on target process termination and stop this subprocess
-		trap 'message --info "Delayed for ${config_key_delay_map["$passed_section"]} second(s) CPU limiting of process '"'$passed_process_name'"' with PID $passed_process_pid has been cancelled due to window termination." ; \
+		trap 'message --info "Delayed for ${config_key_delay_map["$passed_section"]} second(s) CPU limiting of process '"'$passed_process_name'"' with PID $passed_process_pid has been cancelled due to window $passed_window_id termination." ; \
 		kill "$local_sleep_pid"; \
 		exit 0' SIGUSR2
 		# Wait for 'sleep' termination
@@ -26,7 +26,7 @@ background_cpu_limit(){
 	fi
 	# Check for process existence before set CPU limit
 	if check_pid_existence "$passed_process_pid"; then
-		message --info "Process '$passed_process_name' with PID $passed_process_pid has been CPU limited to ${config_key_cpu_limit_map["$passed_section"]}% on unfocus event."
+		message --info "Process '$passed_process_name' with PID $passed_process_pid has been CPU limited to ${config_key_cpu_limit_map["$passed_section"]}% due to window $passed_window_id unfocus event."
 		# Run in background to make subprocess interruptable
 		cpulimit --lazy --limit="$(( "${config_key_cpu_limit_map["$passed_section"]}" * cpu_threads ))" --pid="$passed_process_pid" > /dev/null 2>&1 &
 		# Remember PID of 'cpulimit' to terminate it if needed
@@ -38,11 +38,11 @@ background_cpu_limit(){
 		kill "$local_cpulimit_pid" > /dev/null 2>&1 ; \
 		exit 0' SIGINT SIGTERM
 		# Terminate 'cpulimit' process on focus event and print relevant message
-		trap 'message --info "Process '"'$passed_process_name'"' with PID $passed_process_pid has been CPU unlimited on focus event." ; \
+		trap 'message --info "Process '"'$passed_process_name'"' with PID $passed_process_pid has been CPU unlimited due to window $passed_window_id focus event." ; \
 		kill "$local_cpulimit_pid" > /dev/null 2>&1 ; \
 		exit 0' SIGUSR1
 		# Terminate 'cpulimit' on termination of target and print relevant message
-		trap 'message --info "Process '"'$passed_process_name'"' with PID $passed_process_pid has been CPU unlimited due to window termination." ; \
+		trap 'message --info "Process '"'$passed_process_name'"' with PID $passed_process_pid has been CPU unlimited due to window $passed_window_id termination." ; \
 		kill "$local_cpulimit_pid" > /dev/null 2>&1 ; \
 		exit 0' SIGUSR2
 		# Wait for 'cpulimit' termination
