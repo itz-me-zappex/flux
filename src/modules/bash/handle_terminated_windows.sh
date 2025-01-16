f5525a0645ce778a268d6968acd36fc518e6f322
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-16 23:20:58 +0200

    src/modules/bash/handle_terminated_windows.sh: Single quote process name in message related to window termination.

--- src/modules/bash/handle_terminated_windows.sh
+++ src/modules/bash/handle_terminated_windows.sh
@@ -95,7 +95,7 @@ handle_terminated_windows(){
 			fi
 			# Print message about window termination if its process it does not match with any section
 			if [[ -z "${cache_section_map["$local_terminated_process_pid"]}" ]]; then
-				message --verbose "Window $local_temp_terminated_window_id of process $local_terminated_process_name with PID $local_terminated_process_pid has been terminated."
+				message --verbose "Window $local_temp_terminated_window_id of process '$local_terminated_process_name' with PID $local_terminated_process_pid has been terminated."
 			fi
 			# Unset data in cache related to terminated window
 			unset cache_mismatch_map["$local_terminated_process_pid"] \
