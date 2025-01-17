a9f2c5f2133e34165a97e56d268d4e4d2cd71300
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-17 02:15:01 +0200

    src/main.sh: Bump up version to 1.20.

--- src/main.sh
+++ src/main.sh
@@ -1,5 +1,5 @@
 # Version of daemon shown from 'parse_options()' if '--version' is specified
-daemon_version='1.19.1'
+daemon_version='1.20'
 
 # Set default prefixes for messages
 prefix_error='[x]'
