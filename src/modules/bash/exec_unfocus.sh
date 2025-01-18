eb1833133a371d71ff0b731c85cc3a66444e6f9d
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-19 01:52:54 +0200

    src/modules/bash/exec_unfocus.sh: Fix wrong variable name which is passed to `exec_on_event` (`$passed_event` -> `$passed_end_of_msg`).

--- src/modules/bash/exec_unfocus.sh
+++ src/modules/bash/exec_unfocus.sh
@@ -19,7 +19,7 @@ exec_unfocus(){
 		passed_command_type='default' \
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_exec_unfocus_map["$previous_section"]}" \
-		passed_event="$local_end_of_msg" \
+		passed_end_of_msg="$local_end_of_msg" \
 		exec_on_event
 	fi
 	# Check for previous section match and execute command from 'lazy-exec-unfocus' key if it has been specified and if '--hot' has been unset by daemon after processing opened windows
@@ -27,7 +27,7 @@ exec_unfocus(){
 		passed_command_type='lazy' \
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_lazy_exec_unfocus_map["$previous_section"]}" \
-		passed_event="$local_end_of_msg" \
+		passed_end_of_msg="$local_end_of_msg" \
 		exec_on_event
 	elif [[ "$hot_is_unset" == '1' ]]; then # Needed to avoid execution of lazy unfocus command immediately after unsetting '--hot' option
 		hot_is_unset='2'
