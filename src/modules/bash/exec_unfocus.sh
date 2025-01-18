117bda9d267b7b2e1969511a35b002bccc305c2d
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-19 01:47:38 +0200

    src/modules/bash/exec_unfocus.sh: Add previous window ID to passed end of message.

--- src/modules/bash/exec_unfocus.sh
+++ src/modules/bash/exec_unfocus.sh
@@ -1,5 +1,8 @@
 # Required to execute command from 'exec-unfocus' and 'lazy-exec-unfocus' config keys
 exec_unfocus(){
+	local local_end_of_msg
+	# Set end of message to not repeat it in code
+	local_end_of_msg="due to window $previous_window_id unfocus event"
 	# Export environment variables to interact with them using commands/scripts in 'exec-unfocus' and 'lazy-exec-unfocus' config keys
 	export FLUX_NEW_WINDOW_ID="$window_id" \
 	FLUX_NEW_PROCESS_PID="$process_pid" \
@@ -16,7 +19,7 @@ exec_unfocus(){
 		passed_command_type='default' \
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_exec_unfocus_map["$previous_section"]}" \
-		passed_event='on unfocus event' \
+		passed_event="$local_end_of_msg" \
 		exec_on_event
 	fi
 	# Check for previous section match and execute command from 'lazy-exec-unfocus' key if it has been specified and if '--hot' has been unset by daemon after processing opened windows
@@ -24,7 +27,7 @@ exec_unfocus(){
 		passed_command_type='lazy' \
 		passed_section="$previous_section" \
 		passed_event_command="${config_key_lazy_exec_unfocus_map["$previous_section"]}" \
-		passed_event='on unfocus event' \
+		passed_event="$local_end_of_msg" \
 		exec_on_event
 	elif [[ "$hot_is_unset" == '1' ]]; then # Needed to avoid execution of lazy unfocus command immediately after unsetting '--hot' option
 		hot_is_unset='2'
