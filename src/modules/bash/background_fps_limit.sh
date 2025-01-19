cd04fb797a47e4fa0258549e86d4a4fa6d785f49
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-19 02:10:27 +0200

    src/modules/bash/background_fps_limit.sh: Print delay with which FPS limit has been applied if delay has been specified.

--- src/modules/bash/background_fps_limit.sh
+++ src/modules/bash/background_fps_limit.sh
@@ -1,15 +1,23 @@
 # Required to set specified FPS on unfocus event, runs in background via '&'
 background_fps_limit(){
+	local local_delay
+	# Simplify access to delay specified in config
+	local_delay="${config_key_delay_map["$passed_section"]}"
 	# Wait before set limit and notify user if delay is specified
-	if [[ "${config_key_delay_map["$passed_section"]}" != '0' ]]; then
-		message --verbose "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' will be FPS limited after ${config_key_delay_map["$passed_section"]} second(s) due to window $passed_window_id unfocus event."
-		sleep "${config_key_delay_map["$passed_section"]}"
+	if [[ "$local_delay" != '0' ]]; then
+		message --verbose "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' will be FPS limited after $local_delay second(s) due to window $passed_window_id unfocus event."
+		sleep "$local_delay"
 	fi
 	# Check for process existence before set FPS limit
 	if check_pid_existence "$passed_process_pid"; then
 		# Attempt to change 'fps_limit' in specified MangoHud config file
 		if mangohud_fps_set "${config_key_mangohud_config_map["$passed_section"]}" "${config_key_mangohud_source_config_map["$passed_section"]}" "${config_key_fps_unfocus_map["$passed_section"]}"; then
-			message --info "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' has been limited to ${config_key_fps_unfocus_map["$passed_section"]} FPS due to window $passed_window_id unfocus event."
+			# Define message depending by whether delay is specified or not
+			if [[ "$local_delay" != '0' ]]; then
+				message --info "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' has been limited to ${config_key_fps_unfocus_map["$passed_section"]} FPS due to window $passed_window_id unfocus event."
+			else
+				message --info "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' has been limited to ${config_key_fps_unfocus_map["$passed_section"]} FPS due to window $passed_window_id unfocus event after $local_delay second(s)."
+			fi
 		fi
 	else
 		message --warning "Process matching with section '$passed_section' has been terminated before FPS limiting!"
