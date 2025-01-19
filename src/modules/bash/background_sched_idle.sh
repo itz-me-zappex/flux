4da9afbed1c3de97bd0fad71e5ffea07146bdd59
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-19 02:16:26 +0200

    src/modules/bash/background_sched_idle.sh: Print delay with which `SCHED_IDLE` scheduling policy has been applied if delay has been specified.

--- src/modules/bash/background_sched_idle.sh
+++ src/modules/bash/background_sched_idle.sh
@@ -1,9 +1,12 @@
 # Required to change scheduling policy of process to 'SCHED_IDLE' on unfocus event, runs in background via '&'
 background_sched_idle(){
+	local local_delay
+	# Simplify access to delay specified in config
+	local_delay="${config_key_delay_map["$passed_section"]}"
 	# Wait before change scheduling policy and notify user if delay is specified
-	if [[ "${config_key_delay_map["$passed_section"]}" != '0' ]]; then
-		message --verbose "Scheduling policy of process '$passed_process_name' with PID $passed_process_pid will be changed to 'idle' after ${config_key_delay_map["$passed_section"]} second(s) due to window $passed_window_id unfocus event."
-		sleep "${config_key_delay_map["$passed_section"]}"
+	if [[ "$local_delay" != '0' ]]; then
+		message --verbose "Scheduling policy of process '$passed_process_name' with PID $passed_process_pid will be changed to 'idle' after $local_delay second(s) due to window $passed_window_id unfocus event."
+		sleep "$local_delay"
 	fi
 	# Check for process existence before changing scheduling policy
 	if check_pid_existence "$passed_process_pid"; then
@@ -11,7 +14,12 @@ background_sched_idle(){
 		if ! chrt --idle --pid 0 "$passed_process_pid"; then
 			message --warning "Scheduling policy of process '$passed_process_name' with PID $passed_process_pid cannot be changed to 'idle' due to window $passed_window_id unfocus event!"
 		else
-			message --info "Scheduling policy of process '$passed_process_name' with PID $passed_process_pid has been changed to 'idle' due to window $passed_window_id unfocus event."
+			# Define message depending by whether delay is specified or not
+			if [[ "$local_delay" != '0' ]]; then
+				message --info "Scheduling policy of process '$passed_process_name' with PID $passed_process_pid has been changed to 'idle' due to window $passed_window_id unfocus event."
+			else
+				message --info "Scheduling policy of process '$passed_process_name' with PID $passed_process_pid has been changed to 'idle' due to window $passed_window_id unfocus event after $local_delay second(s)."
+			fi
 		fi
 	else
 		message --warning "Process '$passed_process_name' with PID $passed_process_pid has been terminated before changing scheduling policy to 'idle'!"
