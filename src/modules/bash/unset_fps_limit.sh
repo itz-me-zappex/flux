a64ee0bd4843e8cd0d4398950ae4f25f2f6d54b4
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-20 10:30:40 +0200

    src/modules/bash/unset_fps_limit.sh: Fix double ending of message.

--- src/modules/bash/unset_fps_limit.sh
+++ src/modules/bash/unset_fps_limit.sh
@@ -24,7 +24,7 @@ unset_fps_limit(){
 	if mangohud_fps_set "${config_key_mangohud_config_map["$passed_section"]}" "${config_key_mangohud_source_config_map["$passed_section"]}" "${config_key_fps_focus_map["$passed_section"]}"; then
 		# Print message depending by FPS limit
 		if [[ "${config_key_fps_focus_map["$passed_section"]}" == '0' ]]; then
-			message --info "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' has been FPS unlimited $passed_end_of_msg $passed_end_of_msg."
+			message --info "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' has been FPS unlimited $passed_end_of_msg."
 		elif [[ "${config_key_fps_focus_map["$passed_section"]}" =~ ^[0-9]+$ ]]; then
 			message --info "MangoHud config file '${config_key_mangohud_config_map["$passed_section"]}' from section '$passed_section' has been limited to ${config_key_fps_focus_map["$passed_section"]} FPS $passed_end_of_msg."
 		else
