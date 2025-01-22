27eee9750c86981796780766e5fdbbbd0dbd9ff6
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-01-22 23:53:43 +0200

    src/modules/bash/exec_focus.sh: Pass owner username besides UID.

--- src/modules/bash/exec_focus.sh
+++ src/modules/bash/exec_focus.sh
@@ -10,11 +10,13 @@ exec_focus(){
 		FLUX_PROCESS_PID="$process_pid" \
 		FLUX_PROCESS_NAME="$process_name" \
 		FLUX_PROCESS_OWNER="$process_owner" \
+		FLUX_PROCESS_OWNER_USERNAME="$process_owner_username" \
 		FLUX_PROCESS_COMMAND="$process_command" \
 		FLUX_PREV_WINDOW_ID="$previous_window_id" \
 		FLUX_PREV_PROCESS_PID="$previous_process_pid" \
 		FLUX_PREV_PROCESS_NAME="$previous_process_name" \
 		FLUX_PREV_PROCESS_OWNER="$previous_process_owner" \
+		FLUX_PREV_PROCESS_OWNER_USERNAME="$previous_process_owner_username" \
 		FLUX_PREV_PROCESS_COMMAND="$previous_process_command"
 		# Execute command from 'exec-focus' key if it has been specified
 		if [[ -n "${config_key_exec_focus_map["$section"]}" ]]; then
@@ -37,11 +39,13 @@ exec_focus(){
 		FLUX_PROCESS_PID \
 		FLUX_PROCESS_NAME \
 		FLUX_PROCESS_OWNER \
+		FLUX_PROCESS_OWNER_USERNAME \
 		FLUX_PROCESS_COMMAND \
 		FLUX_PREV_WINDOW_ID \
 		FLUX_PREV_PROCESS_PID \
 		FLUX_PREV_PROCESS_NAME \
 		FLUX_PREV_PROCESS_OWNER \
+		FLUX_PREV_PROCESS_OWNER_USERNAME \
 		FLUX_PREV_PROCESS_COMMAND
 	fi
 }
