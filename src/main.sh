87d5291ca126b49ac6e3f1a7ebf03e317c9fd084
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-02-04 11:48:46 +0200

    src/main.sh, PKGBUILD: Bump up version to 1.20.15.

--- src/main.sh
+++ src/main.sh
@@ -1,5 +1,5 @@
 # Version of daemon shown from 'parse_options()' if '--version' is specified
-daemon_version='1.20.14'
+daemon_version='1.20.15'
 
 # Set default prefixes for messages
 prefix_error='[x]'
