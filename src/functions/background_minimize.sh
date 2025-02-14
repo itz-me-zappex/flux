d2db42d3a6e30d4ace06af88622f807bdfede11f
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-02-14 21:26:59 +0200

    src/functions/background_minimize.sh: Use `window-minimize <hex>` module instead of `xdotool windowminimize <dec>`.

--- src/functions/background_minimize.sh
+++ src/functions/background_minimize.sh
@@ -3,8 +3,8 @@ background_minimize(){
 	# Wait a bit to give daemon a time to interrupt this subprocess in case unfocused window appears focused very quickly (e.g. because of changing window mode)
 	sleep 0.1
 	
-	# Attempt to minimize window using xdotool, window ID should be converted to numeric value from hexadecimal
-	if ! xdotool windowminimize "$(("$passed_window_id"))" > /dev/null 2>&1; then
+	# Attempt to minimize window
+	if ! "${PREFIX}/lib/flux/window-minimize" "$passed_window_id" > /dev/null 2>&1; then
 		message --warning "Unable to forcefully minimize window '$passed_window_id' of process '$passed_process_name' with PID $passed_process_pid on unfocus event!"
 	else
 		message --info "Window '$passed_window_id' of process '$passed_process_name' with PID $passed_process_pid has been forcefully minimized on unfocus event."
