9518907ae4774ad0312eef5fa7fe340d31d54d94
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-02-22 21:07:55 +0200

    src/functions/sched_validate.sh: Print warnings if there are limitations related to rescheduling.

--- src/functions/sched_validate.sh
+++ src/functions/sched_validate.sh
@@ -13,7 +13,8 @@ sched_validate(){
 
     if ! chrt --other --pid 0 "$local_sleep_pid" > /dev/null 2>&1; then
       kill "$local_sleep_pid" > /dev/null 2>&1
-      return 1
+      message --warning "Daemon has insufficient rights to restore scheduling policies, including 'other', add your user to 'flux' group and reboot!"
+      return 0
     else
       sched_change_is_supported='1'
       kill "$local_sleep_pid" > /dev/null 2>&1
@@ -21,9 +22,15 @@ sched_validate(){
 
     # Attempt to execute command with realtime scheduling policy to check whether daemon can restore it on focus or not
     if ! chrt --fifo 1 echo > /dev/null 2>&1; then
-      return 1
+      message --warning "Daemon has insufficient rights to restore 'RR' (round robin) and 'FIFO' (first in first out) scheduling policies, add your user to 'flux' group and reboot!"
+      return 0
     else
       sched_realtime_is_supported='1'
+    fi
+
+    # Warn user about inablity to restore 'SCHED_DEADLINE' in case daemon runs without root rights
+    if (( UID != 0 )); then
+      message --warning "Daemon has insufficient rights to restore 'deadline' scheduling policy! If you need that feature, run daemon as root."
     fi
   fi
 }
