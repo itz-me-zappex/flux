565c84d18eb8f1fdc81fa8a815fe08f530941c88
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-04-10 10:52:08 +0300

    src/functions/daemon_prepare.sh: Improve lock file handling.

--- src/functions/daemon_prepare.sh
+++ src/functions/daemon_prepare.sh
@@ -1,16 +1,44 @@
 # Required to prepare daemon for event reading
 daemon_prepare(){
-  # Exit with an error if lock file and process specified there exists
-  if [[ -f "$lock_file" ]] &&
-     check_pid_existence "$(<"$lock_file")"; then
-    message --error "Multiple instances are not allowed, make sure that daemon is not running before start, if you are really sure, then remove '$(shorten_path "$lock_file")' file."
-    exit 1
-  else
-    # Store PID to lock file to check its existence on next launch (if lock file still exists, e.g. after crash or SIGKILL)
-    if ! echo "$$" > "$lock_file"; then
-      message --error "Unable to create lock file '$(shorten_path "$lock_file")' required to prevent multiple instances!"
+  # Handle already existing lock file
+  if [[ -f "$lock_file" ]]; then
+    # Exit with an error if file is not readable or daemon is already running
+    if check_ro "$lock_file"; then
+      # Do not read file if it is blank
+      if [[ -n "$(<"$lock_file")" ]]; then
+        # Open file and get daemon PID
+        local local_temp_flux_lock_line
+        while read -r local_temp_flux_lock_line; do
+          local local_flux_pid="$local_temp_flux_lock_line"
+          break
+        done < "$lock_file"
+
+        # Exit with an error if daemon already running
+        if check_pid_existence "$local_flux_pid"; then
+          message --error "Multiple instances are not allowed, make sure that daemon is not running before start, if you are really sure, then remove '$(shorten_path "$lock_file")' file."
+          exit 1
+        fi
+      fi
+    else
+      message --error "Unable to read '$(shorten_path "$lock_file")' lock file!"
       exit 1
     fi
+  fi
+
+  # Exit with an error if lock file exists but not accessible for writing
+  if [[ -f "$lock_file" ]] &&
+     ! check_rw "$lock_file"; then
+    message --error "Unable to overwrite '$(shorten_path "$lock_file")' lock file!"
+    exit 1
+  fi
+
+  # Exit with an error with lock file directory is not writable
+  if check_rw "$(dirname "$lock_file")"; then
+    # Store daemon PID to lock file to check its existence on next launch (if lock file still exists, e.g. after crash or SIGKILL)
+    echo "$$" > "$lock_file"
+  else
+    message --error "Unable to create lock file '$(shorten_path "$lock_file")' required to prevent multiple instances!"
+    exit 1
   fi
 
   # Prepare for logging if log file is specified
