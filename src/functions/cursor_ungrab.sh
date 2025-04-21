18c0bf10ef609635903469cbd1ac64fb6279f619
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-04-21 14:52:55 +0300

    src/functions/cursor_ungrab.sh: Do not try terminate `flux-cursor-grab` if it does not exist.

--- src/functions/cursor_ungrab.sh
+++ src/functions/cursor_ungrab.sh
@@ -1,9 +1,13 @@
 # Required to cancel cursor grabbing for window on daemon termination and/or unfocus event
 cursor_ungrab(){
-  if ! kill "${background_focus_cursor_grab_map["$passed_window_xid"]}" > /dev/null 2>&1; then
-    message --warning "Unable to ungrab cursor for window with XID $passed_window_xid of process '$passed_process_name' with PID $passed_process_pid $passed_end_of_msg!"
-  else
-    message --info "Cursor grabbing for window with XID $passed_window_xid of process '$passed_process_name' with PID $passed_process_pid has been cancelled $passed_end_of_msg."
+  local local_background_focus_cursor_grab_pid="${background_focus_cursor_grab_map["$passed_window_xid"]}"
+
+  if check_pid_existence "$local_background_focus_cursor_grab_pid"; then
+    if ! kill "$local_background_focus_cursor_grab_pid" > /dev/null 2>&1; then
+      message --warning "Unable to ungrab cursor for window with XID $passed_window_xid of process '$passed_process_name' with PID $passed_process_pid $passed_end_of_msg!"
+    else
+      message --info "Cursor grabbing for window with XID $passed_window_xid of process '$passed_process_name' with PID $passed_process_pid has been cancelled $passed_end_of_msg."
+    fi
   fi
 
   unset background_focus_cursor_grab_map["$passed_window_xid"]
