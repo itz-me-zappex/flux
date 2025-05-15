108bcc348311a63ecc52974b7ed4ff138bcb3e67
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-05-15 10:50:50 +0300

    src/functions/handle_groups.sh: Overwrite key from group if this key specified after `group` key, do not set value from group if it is blank.

--- src/functions/handle_groups.sh
+++ src/functions/handle_groups.sh
@@ -1,9 +1,15 @@
-# Required to check groups store values to config keys from them
+# Required to check groups and store values to config keys from them
 handle_groups(){
   # Get section name
   local local_temp_section
   for local_temp_section in "${sections_array[@]}"; do
     local local_group="${config_key_group_map["$local_temp_section"]}"
+
+    # Skip if 'group' is not specified
+    if [[ -z "$local_group" ]]; then
+      continue
+    fi
+
     # Check whether section contains 'group' config key or not
     if section_is_group "$local_group"; then
       # Check whether group exits or not
@@ -21,22 +27,114 @@ handle_groups(){
       else
         unset local_group_exists
 
+        local local_group_key_line="$(get_key_line "$local_temp_section" 'group')"
+
         # Store values from group to section
-        config_key_cpu_limit_map["$local_temp_section"]="${config_key_cpu_limit_map["$local_group"]}"
-        config_key_delay_map["$local_temp_section"]="${config_key_delay_map["$local_group"]}"
-        config_key_exec_oneshot_map["$local_temp_section"]="${config_key_exec_oneshot_map["$local_group"]}"
-        config_key_exec_focus_map["$local_temp_section"]="${config_key_exec_focus_map["$local_group"]}"
-        config_key_exec_unfocus_map["$local_temp_section"]="${config_key_exec_unfocus_map["$local_group"]}"
-        config_key_lazy_exec_focus_map["$local_temp_section"]="${config_key_lazy_exec_focus_map["$local_group"]}"
-        config_key_lazy_exec_unfocus_map["$local_temp_section"]="${config_key_lazy_exec_unfocus_map["$local_group"]}"
-        config_key_mangohud_source_config_map["$local_temp_section"]="${config_key_mangohud_source_config_map["$local_group"]}"
-        config_key_mangohud_config_map["$local_temp_section"]="${config_key_mangohud_config_map["$local_group"]}"
-        config_key_fps_unfocus_map["$local_temp_section"]="${config_key_fps_unfocus_map["$local_group"]}"
-        config_key_fps_focus_map["$local_temp_section"]="${config_key_fps_focus_map["$local_group"]}"
-        config_key_idle_map["$local_temp_section"]="${config_key_idle_map["$local_group"]}"
-        config_key_unfocus_minimize_map["$local_temp_section"]="${config_key_unfocus_minimize_map["$local_group"]}"
-        config_key_focus_fullscreen_map["$local_temp_section"]="${config_key_focus_fullscreen_map["$local_group"]}"
-        config_key_focus_cursor_grab_map["$local_temp_section"]="${config_key_focus_cursor_grab_map["$local_group"]}"
+        local local_key_line="$(get_key_line "$local_temp_section" 'cpu-limit')"
+        if [[ -n "${config_key_cpu_limit_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_cpu_limit_map["$local_temp_section"]="${config_key_cpu_limit_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'delay')"
+        if [[ -n "${config_key_delay_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_delay_map["$local_temp_section"]="${config_key_delay_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'exec-oneshot')"
+        if [[ -n "${config_key_exec_oneshot_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_exec_oneshot_map["$local_temp_section"]="${config_key_exec_oneshot_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'exec-focus')"
+        if [[ -n "${config_key_exec_focus_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_exec_focus_map["$local_temp_section"]="${config_key_exec_focus_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'exec-unfocus')"
+        if [[ -n "${config_key_exec_unfocus_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_exec_unfocus_map["$local_temp_section"]="${config_key_exec_unfocus_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'lazy-exec-focus')"
+        if [[ -n "${config_key_lazy_exec_focus_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_lazy_exec_focus_map["$local_temp_section"]="${config_key_lazy_exec_focus_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'lazy-exec-unfocus')"
+        if [[ -n "${config_key_lazy_exec_unfocus_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_lazy_exec_unfocus_map["$local_temp_section"]="${config_key_lazy_exec_unfocus_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'mangohud-source-config')"
+        if [[ -n "${config_key_mangohud_source_config_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_mangohud_source_config_map["$local_temp_section"]="${config_key_mangohud_source_config_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'mangohud-config')"
+        if [[ -n "${config_key_mangohud_config_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_mangohud_config_map["$local_temp_section"]="${config_key_mangohud_config_map["$local_group"]}"
+        fi
+
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'fps-unfocus')"
+        if [[ -n "${config_key_fps_unfocus_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_fps_unfocus_map["$local_temp_section"]="${config_key_fps_unfocus_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'fps-focus')"
+        if [[ -n "${config_key_fps_focus_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_fps_focus_map["$local_temp_section"]="${config_key_fps_focus_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'idle')"
+        if [[ -n "${config_key_idle_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_idle_map["$local_temp_section"]="${config_key_idle_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'unfocus-minimize')"
+        if [[ -n "${config_key_unfocus_minimize_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_unfocus_minimize_map["$local_temp_section"]="${config_key_unfocus_minimize_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'focus-fullscreen')"
+        if [[ -n "${config_key_focus_fullscreen_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_focus_fullscreen_map["$local_temp_section"]="${config_key_focus_fullscreen_map["$local_group"]}"
+        fi
+
+        local local_key_line="$(get_key_line "$local_temp_section" 'focus-cursor-grab')"
+        if [[ -n "${config_key_focus_cursor_grab_map["$local_group"]}" ]] &&
+           (( local_key_line == 0 )) ||
+           (( local_group_key_line > local_key_line )); then
+          config_key_focus_cursor_grab_map["$local_temp_section"]="${config_key_focus_cursor_grab_map["$local_group"]}"
+        fi
 
         # Group key specified in section is no longer needed
         unset config_key_group_map["$local_temp_section"]
