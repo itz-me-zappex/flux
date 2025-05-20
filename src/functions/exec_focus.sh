ebef7e86e8a987a6ae021d4803ecf27582e0e30f
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-05-20 21:52:14 +0300

    src/functions/: exec_focus.sh, exec_oneshot.sh: Move part of code responsible for execution of `exec-oneshot` config key to `exec_oneshot()`.

--- src/functions/exec_focus.sh
+++ src/functions/exec_focus.sh
@@ -19,23 +19,6 @@ exec_focus(){
     FLUX_PREV_PROCESS_OWNER_USERNAME="$previous_process_owner_username" \
     FLUX_PREV_PROCESS_COMMAND="$previous_process_command"
 
-    # Execute command from 'exec-oneshot' key if it has been specified and was not executed before
-    if [[ -n "${config_key_exec_oneshot_map["$section"]}" &&
-          -z "${is_exec_oneshot_executed_map["$process_pid"]}" ]]; then
-      local local_temp_command
-      while read -r local_temp_command ||
-            [[ -n "$local_temp_command" ]]; do
-        passed_command_type='default' \
-        passed_section="$section" \
-        passed_event_command="$local_temp_command" \
-        passed_end_of_msg="due to appearance of window with XID $window_xid of process '$process_name' with PID $process_pid" \
-        passed_event_type='oneshot' \
-        exec_on_event
-      done <<< "${config_key_exec_oneshot_map["$section"]}"
-
-      is_exec_oneshot_executed_map["$process_pid"]='1'
-    fi
-
     # Execute command from 'exec-focus' key if it has been specified
     if [[ -n "${config_key_exec_focus_map["$section"]}" ]]; then
       local local_temp_command
---
+++ src/functions/exec_oneshot.sh
@@ -0,0 +1,47 @@
+# Required to execute command from 'exec-oneshot' config key
+exec_oneshot(){
+  # Export environment variables to interact with those using commands/scripts in 'exec-oneshot' config key
+  export FLUX_WINDOW_XID="$window_xid" \
+  FLUX_PROCESS_PID="$process_pid" \
+  FLUX_PROCESS_NAME="$process_name" \
+  FLUX_PROCESS_OWNER="$process_owner" \
+  FLUX_PROCESS_OWNER_USERNAME="$process_owner_username" \
+  FLUX_PROCESS_COMMAND="$process_command" \
+  FLUX_PREV_WINDOW_XID="$previous_window_xid" \
+  FLUX_PREV_PROCESS_PID="$previous_process_pid" \
+  FLUX_PREV_PROCESS_NAME="$previous_process_name" \
+  FLUX_PREV_PROCESS_OWNER="$previous_process_owner" \
+  FLUX_PREV_PROCESS_OWNER_USERNAME="$previous_process_owner_username" \
+  FLUX_PREV_PROCESS_COMMAND="$previous_process_command"
+
+  # Execute command from 'exec-oneshot' key if it has been specified and was not executed before
+  if [[ -n "${config_key_exec_oneshot_map["$section"]}" &&
+        -z "${is_exec_oneshot_executed_map["$process_pid"]}" ]]; then
+    local local_temp_command
+    while read -r local_temp_command ||
+          [[ -n "$local_temp_command" ]]; do
+      passed_command_type='default' \
+      passed_section="$section" \
+      passed_event_command="$local_temp_command" \
+      passed_end_of_msg="due to appearance of window with XID $window_xid of process '$process_name' with PID $process_pid" \
+      passed_event_type='oneshot' \
+      exec_on_event
+    done <<< "${config_key_exec_oneshot_map["$section"]}"
+
+    is_exec_oneshot_executed_map["$process_pid"]='1'
+  fi
+  
+  # Unset exported variables
+  unset FLUX_WINDOW_XID \
+  FLUX_PROCESS_PID \
+  FLUX_PROCESS_NAME \
+  FLUX_PROCESS_OWNER \
+  FLUX_PROCESS_OWNER_USERNAME \
+  FLUX_PROCESS_COMMAND \
+  FLUX_PREV_WINDOW_XID \
+  FLUX_PREV_PROCESS_PID \
+  FLUX_PREV_PROCESS_NAME \
+  FLUX_PREV_PROCESS_OWNER \
+  FLUX_PREV_PROCESS_OWNER_USERNAME \
+  FLUX_PREV_PROCESS_COMMAND
+}
