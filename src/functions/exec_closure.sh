d4e891dcc138dda8c1b9e45018378eb874294127
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-05-20 23:42:15 +0300

    src/functions/: exec_unfocus.sh, exec_closure.sh: Use `export_unfocus_envvars()` and `unset_unfocus_envvars()`.

--- src/functions/exec_closure.sh
+++ src/functions/exec_closure.sh
@@ -1,18 +1,6 @@
 # Required to execute command from 'exec-closure' config key
 exec_closure(){
-  # Export environment variables to interact with them using commands/scripts in 'exec-closure' config key
-  export FLUX_NEW_WINDOW_XID="$window_xid" \
-  FLUX_NEW_PROCESS_PID="$process_pid" \
-  FLUX_NEW_PROCESS_NAME="$process_name" \
-  FLUX_NEW_PROCESS_OWNER="$process_owner" \
-  FLUX_NEW_PROCESS_OWNER_USERNAME="$process_owner_username" \
-  FLUX_NEW_PROCESS_COMMAND="$process_command" \
-  FLUX_WINDOW_XID="$passed_window_xid" \
-  FLUX_PROCESS_PID="$passed_process_pid" \
-  FLUX_PROCESS_NAME="$passed_process_name" \
-  FLUX_PROCESS_OWNER="$passed_process_owner" \
-  FLUX_PROCESS_OWNER_USERNAME="$passed_process_owner_username" \
-  FLUX_PROCESS_COMMAND="$passed_process_command"
+  export_unfocus_envvars
 
   # Execute command from 'exec-closure' key if it has been specified
   if [[ -n "${config_key_exec_closure_map["$passed_section"]}" ]]; then
@@ -28,17 +16,5 @@ exec_closure(){
     done <<< "${config_key_exec_closure_map["$passed_section"]}"
   fi
   
-  # Unset exported variables
-  unset FLUX_NEW_WINDOW_XID \
-  FLUX_NEW_PROCESS_PID \
-  FLUX_NEW_PROCESS_NAME \
-  FLUX_NEW_PROCESS_OWNER \
-  FLUX_NEW_PROCESS_OWNER_USERNAME \
-  FLUX_NEW_PROCESS_COMMAND \
-  FLUX_WINDOW_XID \
-  FLUX_PROCESS_PID \
-  FLUX_PROCESS_NAME \
-  FLUX_PROCESS_OWNER \
-  FLUX_PROCESS_OWNER_USERNAME \
-  FLUX_PROCESS_COMMAND
+  unset_unfocus_envvars
 }
--- src/functions/exec_unfocus.sh
+++ src/functions/exec_unfocus.sh
@@ -3,19 +3,7 @@ exec_unfocus(){
   # Set end of message to not duplicate it
   local local_end_of_msg="due to unfocus event of window with XID $passed_window_xid of process '$passed_process_name' with PID $passed_process_pid"
 
-  # Export environment variables to interact with them using commands/scripts in 'exec-unfocus' and 'lazy-exec-unfocus' config keys
-  export FLUX_NEW_WINDOW_XID="$window_xid" \
-  FLUX_NEW_PROCESS_PID="$process_pid" \
-  FLUX_NEW_PROCESS_NAME="$process_name" \
-  FLUX_NEW_PROCESS_OWNER="$process_owner" \
-  FLUX_NEW_PROCESS_OWNER_USERNAME="$process_owner_username" \
-  FLUX_NEW_PROCESS_COMMAND="$process_command" \
-  FLUX_WINDOW_XID="$passed_window_xid" \
-  FLUX_PROCESS_PID="$passed_process_pid" \
-  FLUX_PROCESS_NAME="$passed_process_name" \
-  FLUX_PROCESS_OWNER="$passed_process_owner" \
-  FLUX_PROCESS_OWNER_USERNAME="$passed_process_owner_username" \
-  FLUX_PROCESS_COMMAND="$passed_process_command"
+  export_unfocus_envvars
 
   # Execute command from 'exec-unfocus' key if it has been specified
   if [[ -n "${config_key_exec_unfocus_map["$passed_section"]}" ]]; then
@@ -46,17 +34,5 @@ exec_unfocus(){
     done <<< "${config_key_lazy_exec_unfocus_map["$passed_section"]}"
   fi
   
-  # Unset exported variables
-  unset FLUX_NEW_WINDOW_XID \
-  FLUX_NEW_PROCESS_PID \
-  FLUX_NEW_PROCESS_NAME \
-  FLUX_NEW_PROCESS_OWNER \
-  FLUX_NEW_PROCESS_OWNER_USERNAME \
-  FLUX_NEW_PROCESS_COMMAND \
-  FLUX_WINDOW_XID \
-  FLUX_PROCESS_PID \
-  FLUX_PROCESS_NAME \
-  FLUX_PROCESS_OWNER \
-  FLUX_PROCESS_OWNER_USERNAME \
-  FLUX_PROCESS_COMMAND
+  unset_unfocus_envvars
 }
