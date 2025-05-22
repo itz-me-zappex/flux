fe506e94f251effd36e23bbe57d6a8926d2a9633
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-05-22 19:07:32 +0300

    src/functions/unset_groups.sh: Unset `exec-exit`, `exec-exit-focus`, `exec-exit-unfocus` and `exec-closure` config key values in groups.

--- src/functions/unset_groups.sh
+++ src/functions/unset_groups.sh
@@ -5,6 +5,7 @@ unset_groups(){
     if section_is_group "$local_temp_section"; then
       unset config_key_cpu_limit_map["$local_temp_section"] \
       config_key_delay_map["$local_temp_section"] \
+      config_key_exec_closure_map["$local_temp_section"] \
       config_key_exec_oneshot_map["$local_temp_section"] \
       config_key_exec_focus_map["$local_temp_section"] \
       config_key_exec_unfocus_map["$local_temp_section"] \
@@ -18,7 +19,10 @@ unset_groups(){
       config_key_unfocus_minimize_map["$local_temp_section"] \
       config_key_focus_fullscreen_map["$local_temp_section"] \
       config_key_focus_cursor_grab_map["$local_temp_section"] \
-      config_key_group_map["$local_temp_section"]
+      config_key_group_map["$local_temp_section"] \
+      config_key_exec_exit_map["$local_temp_section"] \
+      config_key_exec_exit_focus_map["$local_temp_section"] \
+      config_key_exec_exit_unfocus_map["$local_temp_section"]
     else
       local local_sections_array+=("$local_temp_section")
     fi
