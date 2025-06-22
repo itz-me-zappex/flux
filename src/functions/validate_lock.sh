434c518f755fb4ffe2c4cd27e81945c7b5db933c
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-06-22 15:37:41 +0300

    src/functions/validate_lock.sh: Remove useless code since implementing FIFO usage, add more checks to lock file.

--- src/functions/validate_lock.sh
+++ src/functions/validate_lock.sh
@@ -1,45 +1,45 @@
 # Required to prevent multiple instances from running
 validate_lock(){
-  # Handle already existing lock file
-  if [[ -f "$lock_file" ]]; then
-    # Exit with an error if file is not readable or daemon is already running
-    if check_ro "$lock_file"; then
-      # Do not read file if it is blank
-      if [[ -n "$(<"$lock_file")" ]]; then
-        # Open file and get daemon PID
-        local local_temp_flux_lock_line
-        while read -r local_temp_flux_lock_line ||
-              [[ -n "$local_temp_flux_lock_line" ]]; do
-          local local_flux_pid="$local_temp_flux_lock_line"
-          break
-        done < "$lock_file"
-
-        # Exit with an error if daemon already running
-        if check_pid_existence "$local_flux_pid"; then
-          message --error "Multiple instances are not allowed, make sure that daemon is not running before start, but if you are really sure, then remove '$(shorten_path "$lock_file")' lock file."
-          exit 1
-        fi
-      fi
-    else
-      message --error "Unable to read '$(shorten_path "$lock_file")' lock file!"
+  # Skip checks if file does not exist
+  if [[ -f "$flux_lock_file_path" ]]; then
+    # Exit with an error if lock file is not readable
+    if ! check_ro "$flux_lock_file_path"; then
+      message --error "Unable to read '$(shorten_path "$flux_lock_file_path")' lock file!"
       exit 1
     fi
-  fi
 
-  # Exit with an error if lock file exists but not accessible for writing
-  if [[ -f "$lock_file" ]] &&
-     ! check_rw "$lock_file"; then
-    message --error "Unable to overwrite '$(shorten_path "$lock_file")' lock file!"
+    # Exit with an error if daemon already running
+    if check_pid_existence "$(<"$flux_lock_file_path")"; then
+      message --error "Multiple instances are not allowed, make sure that daemon is not running before start, but if you are really sure, then remove '$(shorten_path "$flux_lock_file_path")' lock file."
+      exit 1
+    fi
+
+    # Exit with an error if lock file exists but not accessible for writing
+    if ! check_rw "$flux_lock_file_path"; then
+      message --error "Unable to overwrite '$(shorten_path "$flux_lock_file_path")' lock file!"
+      exit 1
+    fi
+  elif [[ -e "$flux_lock_file_path" &&
+          ! -f "$flux_lock_file_path" ]]; then
+    message --error "Unable to handle '$(shorten_path "$flux_lock_file_path")', file is expected!"
     exit 1
   fi
 
-  # Exit with an error with lock file directory is not writable
-  local local_lock_file_dir="${lock_file%/*}"
-  if check_rw "$local_lock_file_dir"; then
-    # Store daemon PID to lock file to check its existence on next launch (if lock file still exists, e.g. after crash or SIGKILL)
-    echo "$$" > "$lock_file"
+  # Exit with an error if lock file directory is not writable
+  local local_lock_file_dir="${flux_lock_file_path%/*}"
+  if [[ -d "$local_lock_file_dir" ]]; then
+    if check_rw "$local_lock_file_dir"; then
+      # Store daemon PID to lock file to check its existence on next launch (if lock file still exists, e.g. after crash or SIGKILL)
+      echo "$$" > "$flux_lock_file_path"
+    else
+      message --error "Unable to create '$(shorten_path "$flux_lock_file_path")' lock file required to prevent multiple instances from running, '$(shorten_path "$local_lock_file_dir")' directory is not accessible for read-write operations!"
+      exit 1
+    fi
+  elif [[ -e "$local_lock_file_dir" &&
+          ! -d "$local_lock_file_dir" ]]; then
+    message --error "Unable to create '$(shorten_path "$flux_lock_file_path")' lock file, '$(shorten_path "$local_lock_file_dir")' is expected to be directory!"
+    exit 1
   else
-    message --error "Unable to create lock file '$(shorten_path "$lock_file")' required to prevent multiple instances because '$(shorten_path "$local_lock_file_dir")' directory is not accessible for read-write operations!"
-    exit 1
+    message --error "Unable to create '$(shorten_path "$flux_lock_file_path")' lock file, '$(shorten_path "$local_lock_file_dir")' directory does not exist!"
   fi
 }
