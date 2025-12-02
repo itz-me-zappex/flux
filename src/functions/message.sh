8dce81b62838b5ca1ba7c589b42ebbe5a11b57da
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-12-02 10:13:35 +0200

    src/functions/message.sh: Fix broken output without timestamps.

--- src/functions/message.sh
+++ src/functions/message.sh
@@ -2,31 +2,36 @@
 message(){
   # Get timestamp if that behavior is allowed using '--timestamps' option
   if [[ -n "$allow_timestamps" ]]; then
-    local local_timestamp="%($timestamp_format)T"
+    local local_current_time='-1'
+    local local_timestamp="%($timestamp_format)T %s\n"
 
     if [[ -n "$log_timestamp_format" ]]; then
-      local local_log_timestamp="%($log_timestamp_format)T"
+      local local_log_timestamp="%($log_timestamp_format)T %s\n"
     fi
+  else
+    local local_current_time=''
+    local local_timestamp="%s%s\n"
+    local local_log_timestamp="%s%s\n"
   fi
 
   # Print message depending by passed option
   case "$1" in
   --error )
     shift 1
-    printf "$local_timestamp %s\n" -1 "$prefix_error $*" >&2
+    printf "$local_timestamp" "$local_current_time" "$prefix_error $*" >&2
     local local_log_prefix="$log_prefix_error"
     local local_notification_icon='emblem-error'
   ;;
   --error-opt )
     # Setting '$local_log_prefix' is unneeded because this message will not be logged ever
     shift 1
-    printf "$local_timestamp %s\n" -1 "$prefix_error $*" >&2
+    printf "$local_timestamp" "$local_current_time" "$prefix_error $*" >&2
     echo "$prefix_info Try 'flux --help' for more information."
   ;;
   --info )
     shift 1
     if [[ -z "$quiet" ]]; then
-      printf "$local_timestamp %s\n" -1 "$prefix_info $*"
+      printf "$local_timestamp" "$local_current_time" "$prefix_info $*"
       local local_log_prefix="$log_prefix_info"
       local local_notification_icon='emblem-information'
     else
@@ -36,7 +41,7 @@ message(){
   --verbose )
     shift 1
     if [[ -n "$verbose" ]]; then
-      printf "$local_timestamp %s\n" -1 "$prefix_verbose $*"
+      printf "$local_timestamp" "$local_current_time" "$prefix_verbose $*"
       local local_log_prefix="$log_prefix_verbose"
       local local_notification_icon='emblem-added'
     else
@@ -45,7 +50,7 @@ message(){
   ;;
   --warning )
     shift 1
-    printf "$local_timestamp %s\n" -1 "$prefix_warning $*" >&2
+    printf "$local_timestamp" "$local_current_time" "$prefix_warning $*" >&2
     local local_log_prefix="$log_prefix_warning"
     local local_notification_icon='emblem-warning'
   esac
@@ -54,9 +59,9 @@ message(){
   if [[ -n "$allow_logging" ]]; then
     # Check log file for read-write access before store message to log
     if check_rw "$log"; then
-      printf "$local_timestamp %s\n" -1 "$local_log_prefix $*" >> "$log"
+      printf "$local_timestamp" "$local_current_time" "$local_log_prefix $*" >> "$log"
     else
-      printf "$local_timestamp %s\n" -1 "$prefix_warning Unable to write message to log file '$(shorten_path "$log")', recreate it or check read-write access!" >&2
+      printf "$local_timestamp" "$local_current_time" "$prefix_warning Unable to write message to log file '$(shorten_path "$log")', recreate it or check read-write access!" >&2
     fi
   fi
 
