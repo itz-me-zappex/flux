c1e634640b6addd76d3ecc3abfb4e077f2e0ad0b
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-12-04 10:50:22 +0200

    src/macro/sched_validate.sh: Update messages.

--- src/macro/sched_validate.sh
+++ src/macro/sched_validate.sh
@@ -11,7 +11,7 @@ sched_validate(){
     local local_sleep_pid="$!"
     chrt --idle --pid 0 "$local_sleep_pid" > /dev/null 2>&1
     if ! chrt --other --pid 0 "$local_sleep_pid" > /dev/null 2>&1; then
-      message --warning "Daemon has insufficient rights to change scheduling policies! To make 'idle' config key work, add your user to 'flux' group and reboot."
+      message --warning "Daemon has insufficient rights to change scheduling policies! To make 'idle' config key work and apply optimizations to 'cpulimit' and 'flux-grab-cursor', add your user to 'flux' group and reboot."
     else
       sched_change_is_supported='1'
     fi
@@ -21,8 +21,8 @@ sched_validate(){
     if [[ -n "$sched_change_is_supported" ]] &&
        ! chrt --fifo 1 echo > /dev/null 2>&1; then
       # Adding user to 'flux' group already allows using these scheduling policies
-      # This message will appear in case user configured '/etc/security/limits.conf' manually just to allow support of 'SCHED_IDLE', 'SCHED_BATCH' and 'SCHED_OTHER'
-      message --info "Daemon has insufficient rights to support 'RR' (round robin) and 'FIFO' (first in first out) scheduling policies. If you need this feature, then add your user to 'flux' group and reboot!"
+      # This message will appear in case user configured '/etc/security/limits.conf' manually and did not allow realtime policies
+      message --warning "Daemon has insufficient rights to support 'RR' and 'FIFO' scheduling policies! Optimizations for 'cpulimit' and 'flux-grab-cursor' will not be applied."
     else
       sched_realtime_is_supported='1'
     fi
