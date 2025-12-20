bedb6b1a8cd9f38910599b8b2fbff1970ee0ab44
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2025-12-21 01:06:57 +0200

    src/oneshot/validate_lock.sh: Hide/restore stderr where needed.

--- src/oneshot/validate_lock.sh
+++ src/oneshot/validate_lock.sh
@@ -11,23 +11,29 @@ validate_lock(){
     else
       # Attempt to read lock file
       local local_pid_from_lock_file
+      hide_stderr
       if ! local_pid_from_lock_file="$(<"$flux_lock_file_path")"; then
+        restore_stderr
         local local_shorten_path_result
         shorten_path "$flux_lock_file_path"
         message --error "An error occured trying to read '$local_shorten_path_result' lock file!"
         exit 1
       fi
+      restore_stderr
     fi
 
     # Exit with an error if daemon already running
     if check_pid_existence "$local_pid_from_lock_file"; then
       local local_process_name_from_lock_file
+      hide_stderr
       if ! local_process_name_from_lock_file="$(<"/proc/$local_pid_from_lock_file/comm")"; then
+        restore_stderr
         local local_shorten_path_result
         shorten_path "/proc/$local_pid_from_lock_file/comm"
         message --error "An error occured trying to read '$local_shorten_path_result' to get process name of PID in lock file!"
         exit 1
       fi
+      restore_stderr
 
       if [[ "$local_process_name_from_lock_file" == 'flux' ]]; then
         message --error "Multiple instances are not allowed, make sure that daemon is not running before start!"
