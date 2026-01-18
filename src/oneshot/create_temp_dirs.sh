e584a29f0f75274dda52519d7517cf404fad348a
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2026-01-18 19:59:24 +0200

    src/oneshot/create_temp_dirs.sh: Do not duplicate the code.

--- src/oneshot/create_temp_dirs.sh
+++ src/oneshot/create_temp_dirs.sh
@@ -1,32 +1,21 @@
 # To create temporary directory to store lock and FIFO files
 create_temp_dirs(){
-  # Exit with an error if something is wrong with temporary directory
-  if [[ -e "$flux_temp_dir_path" &&
-        ! -d "$flux_temp_dir_path" ]]; then
-    local local_shorten_path_result
-    shorten_path "$flux_temp_dir_path"
-    message --error "Unable to continue, '$local_shorten_path_result' is expected to be a directory!"
-    exit 1
-  elif [[ ! -d "$flux_temp_dir_path" ]] &&
-       ! mkdir -p "$flux_temp_dir_path" > /dev/null 2>&1; then
-    local local_shorten_path_result
-    shorten_path "$flux_temp_dir_path"
-    message --error "Unable to create '$local_shorten_path_result' temporary directory!"
-    exit 1
-  fi
-
-  # Exit with an error if something is wrong with FIFO's directory
-  if [[ -e "$flux_temp_fifo_dir_path" &&
-        ! -d "$flux_temp_fifo_dir_path" ]]; then
-    local local_shorten_path_result
-    shorten_path "$flux_temp_fifo_dir_path"
-    message --error "Unable to continue, '$local_shorten_path_result' is expected to be a directory!"
-    exit 1
-  elif [[ ! -d "$flux_temp_fifo_dir_path" ]] &&
-       ! mkdir -p "$flux_temp_fifo_dir_path" > /dev/null 2>&1; then
-    local local_shorten_path_result
-    shorten_path "$flux_temp_fifo_dir_path"
-    message --error "Unable to create '$local_shorten_path_result' temporary directory for FIFO files!"
-    exit 1
-  fi
+  # Exit with an error if something is wrong with either temporary
+  # or FIFO's directory
+  local local_temp_directory
+  for local_temp_directory in "$flux_temp_dir_path" "$flux_temp_fifo_dir_path"; do
+    if [[ -e "$local_temp_directory" &&
+          ! -d "$local_temp_directory" ]]; then
+      local local_shorten_path_result
+      shorten_path "$local_temp_directory"
+      message --error "Unable to continue, '$local_shorten_path_result' is expected to be a directory!"
+      exit 1
+    elif [[ ! -d "$local_temp_directory" ]] &&
+         ! mkdir -p "$local_temp_directory" > /dev/null 2>&1; then
+      local local_shorten_path_result
+      shorten_path "$local_temp_directory"
+      message --error "Unable to create '$local_shorten_path_result' temporary directory!"
+      exit 1
+    fi
+  done
 }
