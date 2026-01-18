d5f8031bf8a65f1b3f4ff47297555615feaa08df
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2026-01-18 19:55:34 +0200

    src/oneshot/create_temp_dirs.sh: Handle errors related to FIFO's temp directory properly.

--- src/oneshot/create_temp_dirs.sh
+++ src/oneshot/create_temp_dirs.sh
@@ -2,7 +2,7 @@
 create_temp_dirs(){
   # Exit with an error if something is wrong with temporary directory
   if [[ -e "$flux_temp_dir_path" &&
-     ! -d "$flux_temp_dir_path" ]]; then
+        ! -d "$flux_temp_dir_path" ]]; then
     local local_shorten_path_result
     shorten_path "$flux_temp_dir_path"
     message --error "Unable to continue, '$local_shorten_path_result' is expected to be a directory!"
@@ -15,8 +15,18 @@ create_temp_dirs(){
     exit 1
   fi
 
-  # If it did not fail above, then it should work too, right?...
-  if [[ ! -d "$flux_temp_fifo_dir_path" ]]; then
-    mkdir -p "$flux_temp_fifo_dir_path" > /dev/null 2>&1
+  # Exit with an error if something is wrong with FIFO's directory
+  if [[ -e "$flux_temp_fifo_dir_path" &&
+        ! -d "$flux_temp_fifo_dir_path" ]]; then
+    local local_shorten_path_result
+    shorten_path "$flux_temp_fifo_dir_path"
+    message --error "Unable to continue, '$local_shorten_path_result' is expected to be a directory!"
+    exit 1
+  elif [[ ! -d "$flux_temp_fifo_dir_path" ]] &&
+       ! mkdir -p "$flux_temp_fifo_dir_path" > /dev/null 2>&1; then
+    local local_shorten_path_result
+    shorten_path "$flux_temp_fifo_dir_path"
+    message --error "Unable to create '$local_shorten_path_result' temporary directory for FIFO files!"
+    exit 1
   fi
 }
