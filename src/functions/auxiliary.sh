deb85cb55ff4ed087d83c5dee1d72b55bf3a12ff
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2026-09-04 13:07:13 +0300

    src/functions/auxiliary.sh: Simplify check for odd/even value in `expand_variables()`.

--- src/functions/auxiliary.sh
+++ src/functions/auxiliary.sh
@@ -289,7 +289,7 @@ expand_variables(){
     local local_first_backslash_count="${#local_first_backslashes}"
 
     # '0' if even, '1' if odd
-    local local_backslash_count_is_odd="$(( local_first_backslash_count - local_first_backslash_count / 2 * 2 ))"
+    local local_backslash_count_is_odd="$(( local_first_backslash_count % 2 ))"
 
     if (( local_backslash_count_is_odd == 1 )); then
       # Since we want to ignore escaped variables,
