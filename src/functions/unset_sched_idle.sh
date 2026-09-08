a4eb8c141c986dcd9dde4b8b250edcf9caf7142f
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2026-09-08 21:19:21 +0300

    src/functions/unset_sched_idle.sh: Do not quote sched policy names inside quotes. These quotes were used to separate abbreviation and description (it was in brackets), and descriptions were removed a long time ago to shorten messages.

--- src/functions/unset_sched_idle.sh
+++ src/functions/unset_sched_idle.sh
@@ -19,24 +19,24 @@ unset_sched_idle(){
     case "${sched_previous_policy_map["$passed_pid"]}" in
     'SCHED_FIFO' )
       local local_policy_option='--fifo'
-      local local_policy_name="'FIFO'"
+      local local_policy_name='FIFO'
     ;;
     'SCHED_RR' )
       local local_policy_option='--rr'
-      local local_policy_name="'RR'"
+      local local_policy_name='RR'
     ;;
     'SCHED_OTHER' )
       local local_policy_option='--other'
-      local local_policy_name="'other'"
+      local local_policy_name='other'
     ;;
     'SCHED_BATCH' )
       local local_policy_option='--batch'
-      local local_policy_name="'batch'"
+      local local_policy_name='batch'
     ;;
     'SCHED_DEADLINE' )
       # Setting '$local_policy_option' useless because command
       # for deadline differs greatly
-      local local_policy_name="'deadline'"
+      local local_policy_name='deadline'
     esac
 
     # Define how to restore scheduling policy depending on
@@ -54,9 +54,9 @@ unset_sched_idle(){
 
     # Print message depending on 'chrt' exit code
     if (( $? > 0 )); then
-      message --warning "Unable to restore $local_policy_name scheduling policy for process '$passed_process_name' ($passed_pid) $passed_end_of_msg!"
+      message --warning "Unable to restore '$local_policy_name' scheduling policy for process '$passed_process_name' ($passed_pid) $passed_end_of_msg!"
     else
-      message --info "Scheduling policy $local_policy_name restored for process '$passed_process_name' ($passed_pid) $passed_end_of_msg."
+      message --info "Scheduling policy '$local_policy_name' restored for process '$passed_process_name' ($passed_pid) $passed_end_of_msg."
     fi
     
     # Unset details about previous and applied 'idle' cheduling policies
