433c72ac8ab8a0ceb2cfdebd730cb99d05635215
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2026-09-22 22:05:06 +0300

    src/functions/exec_unfocus.sh: Provide an interface for `exec_unfocus()`.

--- src/functions/exec_unfocus.sh
+++ src/functions/exec_unfocus.sh
@@ -1,29 +1,33 @@
 # Required to execute command from 'exec-unfocus' and 'lazy-exec-unfocus' config keys
 exec_unfocus(){
-  local local_end_of_msg="on window ($passed_window_xid) unfocus event of process '$passed_process_name' ($passed_pid)"
+  local local_window_xid="$1" local_pid="$2" local_section="$3" \
+        local_process_name="$4" local_process_owner="$5" local_process_owner_username="$6" \
+        local_process_command="$7"
 
-  export_unfocus_envvars "$passed_window_xid" "$passed_pid" "$passed_process_name" \
-                         "$passed_process_owner" "$passed_process_owner_username" \
-                         "$passed_process_command"
+  local local_end_of_msg="on window ($local_window_xid) unfocus event of process '$local_process_name' ($local_pid)"
 
-  if [[ -n "${config_key_exec_unfocus_map["$passed_section"]}" ]]; then
+  export_unfocus_envvars "$local_window_xid" "$local_pid" "$local_process_name" \
+                         "$local_process_owner" "$local_process_owner_username" \
+                         "$local_process_command"
+
+  if [[ -n "${config_key_exec_unfocus_map["$local_section"]}" ]]; then
     local local_temp_command
     while read -r local_temp_command ||
           [[ -n "$local_temp_command" ]]; do
-      exec_on_event "$passed_section" "$local_end_of_msg" \
+      exec_on_event "$local_section" "$local_end_of_msg" \
                     'default' 'unfocus' "$local_temp_command"
-    done <<< "${config_key_exec_unfocus_map["$passed_section"]}"
+    done <<< "${config_key_exec_unfocus_map["$local_section"]}"
   fi
 
   # On next loop after handling implicitly opened windows
-  if [[ -n "${config_key_lazy_exec_unfocus_map["$passed_section"]}" &&
+  if [[ -n "${config_key_lazy_exec_unfocus_map["$local_section"]}" &&
         -n "$allow_lazy_commands" ]]; then
     local local_temp_command
     while read -r local_temp_command ||
           [[ -n "$local_temp_command" ]]; do
-      exec_on_event "$passed_section" "$local_end_of_msg" \
+      exec_on_event "$local_section" "$local_end_of_msg" \
                     'lazy' 'unfocus' "$local_temp_command"
-    done <<< "${config_key_lazy_exec_unfocus_map["$passed_section"]}"
+    done <<< "${config_key_lazy_exec_unfocus_map["$local_section"]}"
   fi
   
   unset_envvars
