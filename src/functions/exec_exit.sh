aa98f42c758faf2134643a7a03f231241cb97590
itz-me-zappex <85901674+itz-me-zappex@users.noreply.github.com>
2026-09-22 22:02:22 +0300

    src/functions/exec_exit.sh: Provide an interface for `exec_exit()`.

--- src/functions/exec_exit.sh
+++ src/functions/exec_exit.sh
@@ -1,41 +1,45 @@
 # To execute commands from 'exec-exit', 'exec-exit-focus' and
 # 'exec-exit-unfocus' config keys
 exec_exit(){
-  if [[ -z "$passed_section" ]]; then
+  local local_window_xid="$1" local_pid="$2" local_section="$3" local_process_name="$4" \
+        local_process_owner="$5" local_process_owner_username="$6" local_process_command="$7" \
+        local_focused_section="$8" local_end_of_msg="$9"
+
+  if [[ -z "$local_section" ]]; then
     return 0
   fi
 
-  export_unfocus_envvars "$passed_window_xid" "$passed_pid" "$passed_process_name" \
-                         "$passed_process_owner" "$passed_process_owner_username" \
-                         "$passed_process_command"
+  export_unfocus_envvars "$local_window_xid" "$local_pid" "$local_process_name" \
+                         "$local_process_owner" "$local_process_owner_username" \
+                         "$local_process_command"
 
-  if [[ -n "${config_key_exec_exit_map["$passed_section"]}" ]]; then
+  if [[ -n "${config_key_exec_exit_map["$local_section"]}" ]]; then
     local local_temp_command
     while read -r local_temp_command ||
           [[ -n "$local_temp_command" ]]; do
-      exec_on_event "$passed_section" "$passed_end_of_msg" \
+      exec_on_event "$local_section" "$local_end_of_msg" \
                     'default' 'exit' "$local_temp_command"
-    done <<< "${config_key_exec_exit_map["$passed_section"]}"
+    done <<< "${config_key_exec_exit_map["$local_section"]}"
   fi
 
-  if [[ "$passed_focused_section" == "$passed_section" &&
-        -n "${config_key_exec_exit_focus_map["$passed_section"]}" ]]; then
+  if [[ "$local_focused_section" == "$local_section" &&
+        -n "${config_key_exec_exit_focus_map["$local_section"]}" ]]; then
     local local_temp_command
     while read -r local_temp_command ||
           [[ -n "$local_temp_command" ]]; do
-      exec_on_event "$passed_section" "$passed_end_of_msg" \
+      exec_on_event "$local_section" "$local_end_of_msg" \
                     'default' 'exit focus' "$local_temp_command"
-    done <<< "${config_key_exec_exit_focus_map["$passed_section"]}"
+    done <<< "${config_key_exec_exit_focus_map["$local_section"]}"
   fi
 
-  if [[ "$passed_focused_section" != "$passed_section" && 
-        -n "${config_key_exec_exit_unfocus_map["$passed_section"]}" ]]; then
+  if [[ "$local_focused_section" != "$local_section" && 
+        -n "${config_key_exec_exit_unfocus_map["$local_section"]}" ]]; then
     local local_temp_command
     while read -r local_temp_command ||
           [[ -n "$local_temp_command" ]]; do
-      exec_on_event "$passed_section" "$passed_end_of_msg" \
+      exec_on_event "$local_section" "$local_end_of_msg" \
                     'default' 'exit unfocus' "$local_temp_command"
-    done <<< "${config_key_exec_exit_unfocus_map["$passed_section"]}"
+    done <<< "${config_key_exec_exit_unfocus_map["$local_section"]}"
   fi
   
   unset_envvars
